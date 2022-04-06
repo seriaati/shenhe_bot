c664af8dde18e1cb97c1e640aaf250aab1cf86a8
seria_ati <seria.ati@gmail.com>
2022-04-07 06:47:13 +0800

    Update othercmd.py

--- cmd/othercmd.py
+++ cmd/othercmd.py
@@ -16,8 +16,8 @@ async def flash(ctx):
     await ctx.send("https://media.discordapp.net/attachments/823440627127287839/960177992942891038/IMG_9555.jpg")
 
 @commands.command()
-async def randnumber(ctx, arg1: int, arg2: int):
-    value = randint(arg1, arg2)
+async def randnumber(ctx, arg1, arg2):
+    value = randint(int(arg1), int(arg2))
     await ctx.send(str(value))
 
 def setup(bot):
