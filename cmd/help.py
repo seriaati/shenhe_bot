227d694c3f01e1f3d3093b3a5aaa27caabdd1653
seria_ati <seria.ati@gmail.com>
2022-04-07 10:39:10 +0800

    Update help.py

--- cmd/help.py
+++ cmd/help.py
@@ -21,7 +21,7 @@ async def help(ctx):
 async def adminhelp(ctx):
     embedHelp = global_vars.defaultEmbed("管理員指令", "`!adminhelp`呼叫此界面\n`!dm`私訊提醒功能\n`!stuck`hoyo資料沒打開\n`!reload`更新封包\n`!check_loop`打開私訊功能")
     global_vars.setFooter(embedHelp)
-    await ctx.send(embedHelp)
+    await ctx.send(embed=embedHelp)
 
 def setup(bot):
     bot.add_command(help)
