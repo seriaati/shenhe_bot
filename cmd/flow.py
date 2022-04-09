ce9bf2623c4005aeabecca5355022b458409c58c
seria_ati <seria.ati@gmail.com>
2022-04-09 13:21:54 +0800

    Create flow.py

---
+++ cmd/flow.py
@@ -0,0 +1,30 @@
+import getpass
+owner = getpass.getuser()
+import sys 
+sys.path.append(f'C:/Users/{owner}/shenhe_bot/asset')
+import os, discord, asyncio, genshin, yaml, datetime
+import global_vars
+global_vars.Global()
+from discord.ext import commands
+
+with open(f'C:/Users/{owner}/shenhe_bot/asset/flow.yaml', encoding = 'utf-8') as file:
+    users = yaml.full_load(file)
+
+class FlowCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def flow(self, ctx):
+    	found = False
+    	for user in users:
+    		if user['discordID']==ctx.author.id:
+    			found = True
+    			await ctx.send(f"使用者: {user['name']}\nflow幣: {user['flow']}")
+    	if found == False:
+    		newUser = {'name': ctx.author, 'discordID': ctx.author.id, 'flow': 0}
+    		users.append(newUser)
+    		with open(f'C:/Users/{owner}/shenhe_bot/asset/flow.yaml', 'w', encoding = 'utf-8') as file:
+                yaml.dump(users, file)
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
