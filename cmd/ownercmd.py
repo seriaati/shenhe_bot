bb820f2b2297a04950c9ed37acfdfe5499b2d8a3
seria_ati <seria.ati@gmail.com>
2022-04-10 10:23:27 +0800

    Update ownercmd.py

--- cmd/ownercmd.py
+++ cmd/ownercmd.py
@@ -6,6 +6,7 @@ import discord, asyncio, yaml, datetime
 import global_vars
 global_vars.Global()
 from discord.ext import commands
+from discord.ext.forms import Form
 
 with open(f'C:/Users/{owner}/shenhe_bot/asset/accounts.yaml', encoding = 'utf-8') as file:
     users = yaml.full_load(file)
@@ -15,94 +16,25 @@ class OwnerCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @commands.is_owner()
+    @commands.has_role("小雪團隊")
     async def newuser(self, ctx):
-        await ctx.send("原神uid?")
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-        try:
-            message = await self.bot.wait_for('message', 
-                timeout= 30.0, 
-                check= check) #等待答案，最多30秒
-        except asyncio.TimeoutError:
-            await ctx.send(global_vars.timeOutErrorMsg) #傳送錯誤訊息（超過30秒
-            return
-        else:
-            uid = message.content
-            await message.delete()
-            await ctx.send("ltuid?")
-            def check(m):
-                return m.author == ctx.author and m.channel == ctx.channel
-            try:
-                message = await self.bot.wait_for('message', 
-                    timeout= 30.0, 
-                    check= check) #等待答案，最多30秒
-            except asyncio.TimeoutError:
-                await ctx.send(global_vars.timeOutErrorMsg) #傳送錯誤訊息（超過30秒
-                return
-            else:
-                ltuid = message.content
-                await message.delete()
-                await ctx.send("ltoken?")
-                def check(m):
-                    return m.author == ctx.author and m.channel == ctx.channel
-                try:
-                    message = await self.bot.wait_for('message', 
-                        timeout= 30.0, 
-                        check= check) #等待答案，最多30秒
-                except asyncio.TimeoutError:
-                    await ctx.send(global_vars.timeOutErrorMsg) #傳送錯誤訊息（超過30秒
-                    return
-                else:
-                    ltoken = message.content
-                    await message.delete()
-                    await ctx.send("用戶名?")
-                    def check(m):
-                        return m.author == ctx.author and m.channel == ctx.channel
-                    try:
-                        message = await self.bot.wait_for('message', 
-                            timeout= 30.0, 
-                            check= check) #等待答案，最多30秒
-                    except asyncio.TimeoutError:
-                        await ctx.send(global_vars.timeOutErrorMsg) #傳送錯誤訊息（超過30秒
-                        return
-                    else:
-                        name = message.content
-                        await message.delete()
-                        await ctx.send("discord ID?")
-                        def check(m):
-                            return m.author == ctx.author and m.channel == ctx.channel
-                        try:
-                            message = await self.bot.wait_for('message', 
-                                timeout= 30.0, 
-                                check= check) #等待答案，最多30秒
-                        except asyncio.TimeoutError:
-                            await ctx.send(global_vars.timeOutErrorMsg) #傳送錯誤訊息（超過30秒
-                            return
-                        else:
-                            discordID = message.content
-                            await message.delete()
-                            await ctx.send(f"正確打yes\n\nuid: {uid}\nltuid: {ltuid}\nltoken: {ltoken}\nusername: {name}\ndiscordID: {discordID}")
-                            try:
-                                message = await self.bot.wait_for('message', 
-                                    timeout= 30.0, 
-                                    check= check) #等待答案，最多30秒
-                            except asyncio.TimeoutError:
-                                await ctx.send(global_vars.timeOutErrorMsg) #傳送錯誤訊息（超過30秒
-                                return
-                            else:
-                                answer = message.content
-                                await message.delete()
-                                if answer == "yes":
-                                    dateNow = datetime.datetime.now()
-                                    newUser = {'name': str(name), 'uid': int(uid), 'discordID': int(discordID), 'ltoken': str(ltoken), 'ltuid': int(ltuid), 'dm': True, 'dmCount': 0, 'dmDate': dateNow}
-                                    users.append(newUser)
-                                    with open(f'C:/Users/{owner}/shenhe_bot/asset/accounts.yaml', 'w', encoding = 'utf-8') as file:
-                                        yaml.dump(users, file)
-                                    await ctx.send(f"已新增{name}")
-                                else:
-                                    await ctx.send("已退出")
-                                    return
+        form = Form(ctx, '新增帳號設定流程')
+        form.add_question('原神UID?', 'uid')
+        form.add_question('用戶名?', 'name')
+        form.add_question('discord ID?', 'discordID')
+        form.add_question('ltuid?', 'ltuid')
+        form.add_question('ltoken?', 'ltoken')
 
+        form.edit_and_delete(True)
+        form.set_timeout(60)
+        await form.set_color("0xa68bd3")
+        result = await form.start()
+        dateNow = datetime.datetime.now()
+        newUser = {'name': str(result.name), 'uid': int(result.uid), 'discordID': int(result.discordID), 'ltoken': str(result.ltoken), 'ltuid': int(result.ltuid), 'dm': True, 'dmCount': 0, 'dmDate': dateNow}
+        users.append(newUser)
+        with open(f'C:/Users/{owner}/shenhe_bot/asset/accounts.yaml', 'w', encoding = 'utf-8') as file:
+            yaml.dump(users, file)
+        await ctx.send(f"已新增該帳號")
+        
 def setup(bot):
     bot.add_cog(OwnerCog(bot))
