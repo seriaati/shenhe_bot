35905e2adae59c9dbe3822e352787d12f63cb95e
seria_ati <seria.ati@gmail.com>
2022-04-14 17:56:31 +0800

    att update

--- cmd/att.py
+++ cmd/att.py
@@ -1,12 +1,21 @@
-import gspread
-import discord
+import gspread, discord, re
 from discord.ext import commands
 from discord.ext.forms import Form
 
 sa = gspread.service_account()
 sh = sa.open("Genshin")
 wks = sh.worksheet("4/14")
-titleCells = ["b3", "f3", "b9", "f9", "b15", "f15", "b21", "f21", "j21"]
+teamCol = ["d", "h", "l"]
+teamRow = ["3", "9", "15", "21"]
+captainRow = ["4", "10", "16", "22"]
+teamCells = []
+for col in teamCol:
+	for row in teamRow:
+		teamCells.append(col+row)
+captainCells = []
+for col in teamCol:
+	for row in captainRow:
+		captainCells.append(col+row)
 
 class AttendCog(commands.Cog):
 	def __init__(self, bot):
@@ -27,7 +36,14 @@ class AttendCog(commands.Cog):
 		form.set_timeout(60)
 		await form.set_color("0xa68bd3")
 		result = await form.start()
-		
+		for team in teamCells:
+			if result.title == wks.acell(team).value:
+				cellWord = " ".join(re.findall("[a-zA-Z]+", str(team)))
+				cellNum = re.findall(r'\b\d+\b', str(team))
+				cell = cellWord+str(cellNum)
+				if ctx.author.id == wks.acell(cell):
+					await ctx.send("true")
+
 
 def setup(bot):
 	bot.add_cog(AttendCog(bot))
