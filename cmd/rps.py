50c28494be2a3f3f98c2f3336df4202ffcaeec15
seria_ati <seria.ati@gmail.com>
2022-04-14 09:20:46 +0800

    Update rps.py

--- cmd/rps.py
+++ cmd/rps.py
@@ -28,8 +28,14 @@ class RPSCog(commands.Cog):
     async def on_raw_reaction_add(self, ev: discord.RawReactionActionEvent):
         if ev.user_id != self.bot.user.id and ev.message_id != 963970685770805288:
             await self.bot.http.delete_message(ev.channel_id, ev.message_id)
-            msg = "「我輸了嗎...?」 :anger:" if str(ev.emoji) == rand.choice(self.reactions) \
-                else "「這個叫做剪刀石頭布的遊戲好像挺好玩...」"
+            msg = ""
+            if str(ev.emoji) == rand.choice(self.reactions):
+                # msg = "「我輸了嗎...?」 :anger:"
+                msg = "=="
+            elif str(ev.emoji) != rand.choice(self.reactions):
+                msg = "!="
+            else:
+                msg = "else"
             embed = global_vars.defaultEmbed("誰贏了呢?", f"{msg}\n你出了: {str(ev.emoji)}\n申鶴出了: {rand.choice(self.reactions)}")
             global_vars.setFooter(embed)
             win = False
@@ -55,8 +61,12 @@ class RPSCog(commands.Cog):
                 if 'rps' not in user:
                     print("rps no in user")
                     user['rps'] = 1
+                    with open(f'C:/Users/{owner}/shenhe_bot/asset/flow.yaml', 'w', encoding = 'utf-8') as file:
+                        yaml.dump(users, file)
                 if 'rpsDate' not in user:
                     user['rpsDate'] = dateNow
+                    with open(f'C:/Users/{owner}/shenhe_bot/asset/flow.yaml', 'w', encoding = 'utf-8') as file:
+                        yaml.dump(users, file)
                 diffDays = abs((dateNow - user['rpsDate']).days)
                 if diffDays >= 1 and user['rps']<= 10 and win == True:
                     user['flow'] += 1
