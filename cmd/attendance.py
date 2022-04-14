64c88cd5b84440b5b1177d14eb57703c9724475f
seria_ati <seria.ati@gmail.com>
2022-04-14 17:11:00 +0800

    teams get

--- cmd/attendance.py
+++ cmd/attendance.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 sa = gspread.service_account()
 sh = sa.open("Genshin")
 wks = sh.worksheet("4/14")
+titleCells = ["b3", "f3", "b9", "f9", "b15", "f15", "b21", "f21", "j21"]
 
 class AttendCog(commands.Cog):
 	def __init__(self, bot):
@@ -13,6 +14,13 @@ class AttendCog(commands.Cog):
 	@commands.command()
 	async def get(self, ctx, arg):
 		await ctx.send(wks.acell(arg).value)
+
+	@commands.command()
+	async def teams(self, ctx):
+		titleStr = ""
+		for title in titleCells:
+			titleStr += f"= {wks.acell(title)}\n"
+		await ctx.send(titleStr)
 
 def setup(bot):
 	bot.add_cog(AttendCog(bot))
