863c2b733c532ecbfe40ce5a04190dbf1cfd9e46
seria_ati <seria.ati@gmail.com>
2022-04-14 08:18:13 +0800

    rps

--- cmd/rps.py
+++ cmd/rps.py
@@ -1,20 +1,23 @@
 import random as rand
 import discord
+import getpass
+owner = getpass.getuser()
+import sys 
+sys.path.append(f'C:/Users/{owner}/shenhe_bot/asset')
+import global_vars
 from discord.ext import commands
 
-from utils import EmbedBuilder
-
-class Commands(commands.Cog):
+class RPSCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    reactions = ['<:fasrock:701479480618909726>', '<:faspaper:701479480543150090>', '<:fasscissors:701479480593744002>']
+    reactions = [':fist:', ':hand_splayed:', ':v:']
 
-    @commands.command(name='rps')
-    async def _rps(self, ctx):
-        msg = await ctx.send(embed=EmbedBuilder.embed(title=f"Rock Paper Scissor",
-                                                      description=f"Please react, which item you want to choose..",
-                                                      color=EmbedBuilder.randcolor()), delete_after=60)
+    @commands.command()
+    async def rps(self, ctx):
+        embed = global_vars.defaultEmbed("剪刀石頭布", "請選一個!")
+        global_vars.setFooter(embed)
+        msg = await ctx.send(embed=embed)
         for reaction in self.reactions: await msg.add_reaction(reaction)
         await ctx.message.delete()
 
@@ -22,11 +25,11 @@ class Commands(commands.Cog):
     async def on_raw_reaction_add(self, ev: discord.RawReactionActionEvent):
         if ev.user_id != self.bot.user.id:
             await self.bot.http.delete_message(ev.channel_id, ev.message_id)
-            msg = "The bot has won.... Try again!" if str(ev.emoji) == rand.choice(self.reactions) \
-                else "You have won... Congratulation!"
-            await self.bot.get_channel(ev.channel_id).send(
-                embed=EmbedBuilder.embed(title=f"Rock Paper Scissor", description=msg, color=EmbedBuilder.randcolor()),
-                delete_after=60)
+            msg = "哈哈, 申鶴贏了!" if str(ev.emoji) == rand.choice(self.reactions) \
+                else "居然輸給你了..."
+            embed = global_vars.defaultEmbed("剪刀石頭布", msg)
+            global_vars.setFooter(embed)
+            await self.bot.get_channel(ev.channel_id).send(embed=embed)
 
 def setup(bot: commands.Bot):
-    bot.add_cog(Commands(bot))
+    bot.add_cog(RPSCog(bot))
--- run.py
+++ run.py
@@ -34,7 +34,7 @@ initial_extensions = [
 "cmd.group",
 "cmd.redeem",
 "cmd.flow",
-# "cmd.rps"
+"cmd.rps"
 ]
 if __name__ == '__main__':
     for extension in initial_extensions:
