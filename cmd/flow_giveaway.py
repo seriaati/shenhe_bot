336e19de7d24b635f5552daf60075624e38c8c56
seria_ati <seria.ati@gmail.com>
2022-04-18 10:09:30 +0800

    clean up code

--- cmd/flow_giveaway.py
+++ cmd/flow_giveaway.py
@@ -1,11 +1,16 @@
 import getpass
+
 owner = getpass.getuser()
-import sys 
+import sys
+
 sys.path.append(f'C:/Users/{owner}/shenhe_bot/asset')
-import discord, yaml
+import discord
 import global_vars
+import yaml
+
 global_vars.Global()
 from discord.ext import commands
+
 
 class FlowGiveawayCog(commands.Cog):
     def __init__(self, bot):
