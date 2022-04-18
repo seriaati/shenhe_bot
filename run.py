1d2cc564a80bcdd2ab52b8c490f97f817b94ddf0
seria_ati <seria.ati@gmail.com>
2022-04-18 19:28:33 +0800

    fix loops

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ async def claimLoop():
 
 @tasks.loop(seconds=600)
 async def checkLoop():
-    for user in users():
+    for user in users:
         userID = user
         try:
             cookies = {"ltuid": users[userID]['ltuid'],
