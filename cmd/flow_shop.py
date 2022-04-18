bb6a78054aef71a8a4882c443c24272ca1d8e434
seria_ati <seria.ati@gmail.com>
2022-04-18 09:18:43 +0800

    flow updates

--- cmd/flow_shop.py
+++ cmd/flow_shop.py
@@ -2,7 +2,7 @@ import getpass
 owner = getpass.getuser()
 import sys 
 sys.path.append(f'C:/Users/{owner}/shenhe_bot/asset')
-import discord, yaml, cmd.flow
+import discord, yaml
 import global_vars
 global_vars.Global()
 from discord.ext import commands
