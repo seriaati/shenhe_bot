4e99a2d11fe34f26c1d6d588e84c596d4e162504
seria_ati <seria.ati@gmail.com>
2022-04-18 12:51:28 +0800

    moved tasks into genshin

--- cmd/genshin.py
+++ cmd/genshin.py
@@ -1,3 +1,4 @@
+from asyncio import tasks
 import datetime
 import getpass
 import sys
@@ -26,6 +27,85 @@ with open(f'C:/Users/{owner}/shenhe_bot/asset/accounts.yaml', 'r', encoding='utf
 class GenshinCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @tasks.loop(hours=24)
+    async def claimLoop():
+        for user in users:
+            cookies = {"ltuid": user['ltuid'], "ltoken": user['ltoken']}
+            username = user['name']
+            client = genshin.Client(cookies)
+            client.default_game = genshin.Game.GENSHIN
+            client.lang = "zh-tw"
+            if user['name'] == "小雪":
+                client.uids[genshin.Game.GENSHIN] = 901211014
+            signed_in, claimed_rewards = await client.get_reward_info()
+            try:
+                reward = await client.claim_daily_reward()
+            except genshin.AlreadyClaimed:
+                print(f"{user['name']} already claimed")
+            else:
+                print(f"claimed for {user['name']}")
+
+    @tasks.loop(seconds=600)
+    async def checkLoop():
+        for user in users:
+            if isinstance(user, type(None)) == True:
+                print(f"use is NoneType: {user['name']}")
+            else:
+                try:
+                    cookies = {"ltuid": user['ltuid'],
+                               "ltoken": user['ltoken']}
+                    uid = user['uid']
+                    username = user['name']
+                    userObj = bot.get_user(user['discordID'])
+                    client = genshin.Client(cookies)
+                    client.default_game = genshin.Game.GENSHIN
+                    client.lang = "zh-tw"
+                    if user['name'] == "小雪":
+                        client.uids[genshin.Game.GENSHIN] = 901211014
+                    notes = await client.get_notes(uid)
+                    resin = notes.current_resin
+                    dateNow = datetime.datetime.now()
+                    diff = dateNow - user['dmDate']
+                    diffHour = diff.total_seconds() / 3600
+                    if resin >= 140 and user['dm'] == True and user['dmCount'] < 3 and diffHour >= 1:
+                        time = notes.remaining_resin_recovery_time
+                        hours, minutes = divmod(time // 60, 60)
+                        fullTime = datetime.datetime.now() + datetime.timedelta(hours=hours)
+                        printTime = '{:%H:%M}'.format(fullTime)
+                        embed = global_vars.defaultEmbed(f"<:danger:959469906225692703>: 目前樹脂數量已經超過140!",
+                                                         f"<:resin:956377956115157022> 目前樹脂: {notes.current_resin}/{notes.max_resin}\n於 {hours:.0f} 小時 {minutes:.0f} 分鐘後填滿(即{printTime})\n註: 不想收到這則通知打`!dm off`, 想重新打開打`!dm on`\n註: 部份指令, 例如`!check`可以在私訊運作")
+                        global_vars.setFooter(embed)
+                        await userObj.send(embed=embed)
+                        user['dmCount'] += 1
+                        user['dmDate'] = dateNow
+                        with open(f'C:/Users/{owner}/shenhe_bot/asset/accounts.yaml', 'w', encoding='utf-8') as file:
+                            yaml.dump(users, file)
+                    elif resin < 140:
+                        user['dmCount'] = 0
+                        with open(f'C:/Users/{owner}/shenhe_bot/asset/accounts.yaml', 'w', encoding='utf-8') as file:
+                            yaml.dump(users, file)
+                except genshin.errors.InvalidCookies:
+                    pass
+                except AttributeError:
+                    print(f"{user['name']} 可能退群了")
+
+    # 等待申鶴準備
+    @checkLoop.before_loop
+    async def beforeLoop():
+        print('waiting...')
+        await bot.wait_until_ready()
+
+    @claimLoop.before_loop
+    async def wait_until_1am():
+        now = datetime.datetime.now().astimezone()
+        next_run = now.replace(hour=1, minute=0, second=0)
+        if next_run < now:
+            next_run += datetime.timedelta(days=1)
+        await discord.utils.sleep_until(next_run)
+
+    checkLoop.start()
+    claimLoop.start()
 
     @commands.command()
     async def check(self, ctx, *, member: discord.Member = None):
