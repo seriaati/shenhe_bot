1d23d3ddf02f2d16b787bc0a9c680af486e47282
seria_ati <seria.ati@gmail.com>
2022-04-20 19:04:14 +0800

    is mora back in log!?

--- cmd/genshin.py
+++ cmd/genshin.py
@@ -292,8 +292,8 @@ class GenshinCog(commands.Cog):
         moraLog = ""
         async for action in client.diary_log(limit=25):
             primoLog = primoLog+f"{action.action} - {action.amount} 原石"+"\n"
-        # async for action in client.diary_log(limit=25, type=genshin.models.DiaryType.MORA):
-        #     moraLog = moraLog+f"{action.action} - {action.amount} 摩拉"+"\n"
+        async for action in client.diary_log(limit=25, type=genshin.models.DiaryType.MORA):
+            moraLog = moraLog+f"{action.action} - {action.amount} 摩拉"+"\n"
         embedPrimo = defaultEmbed(
             f"<:primo:958555698596290570> 最近25筆原石紀錄", f"{primoLog}")
         setFooter(embedPrimo)
