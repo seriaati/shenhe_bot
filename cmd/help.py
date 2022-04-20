47b7fa3be7e5928a5df6bb4373945db88626004b
seria_ati <seria.ati@gmail.com>
2022-04-20 16:54:44 +0800

    removed !flow from help in call

--- cmd/help.py
+++ cmd/help.py
@@ -12,7 +12,7 @@ class HelpCog(commands.Cog):
         embed.add_field(name="➜ 原神資料(需註冊)",
                         value="`!register`註冊帳號\n`!users`所有帳號和uid\n`!check`樹脂等資料\n`!abyss`深淵資料\n`!stats`原神其他資料\n`!claim`領取網頁登入獎勵\n`!diary`本月原石摩拉獲取量\n`!log`詳細原石摩拉獲取資訊\n`!char`查看角色\n`!area`區域探索度\n`!today`今日原石摩拉收入")
         embed.add_field(name="➜ 呼叫!",
-                        value="`!airplane`呼叫飛機仔!\n`!rabbit`呼叫兔兔!\n`!snow`小雪國萬歲!\n`!小雪`呼叫小雪!\n`!turtle`呼叫律律龜!\n`!flow`呼叫flow!\n`!tedd`呼叫tedd!\n`!ttos`呼叫土司!\n`!maple`呼叫楓!")
+                        value="`!airplane`呼叫飛機仔!\n`!rabbit`呼叫兔兔!\n`!snow`小雪國萬歲!\n`!小雪`呼叫小雪!\n`!turtle`呼叫律律龜!\n`!tedd`呼叫tedd!\n`!ttos`呼叫土司!\n`!maple`呼叫楓!")
         embed.add_field(name="➜ 趣味",
                         value="`!cute <人物>`可愛~\n`!say`讓申鶴說話\n`!flash`防放閃指令\n`!vote`發起投票\n`!marry`結婚owo")
         embed.add_field(name="➜ 其他功能",
