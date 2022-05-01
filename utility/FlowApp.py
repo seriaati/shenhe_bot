2877ebfa2841056d44c04327474c7e8915ccfe95
seria_ati <seria.ati@gmail.com>
2022-05-01 23:00:37 +0800

    added flow app

---
+++ utility/FlowApp.py
@@ -0,0 +1,48 @@
+from datetime import date
+import yaml
+
+from utility.utils import errEmbed, log
+class FlowApp:
+    def __init__(self) -> None:
+        with open('data/flow.yaml', 'r', encoding="utf-8") as f:
+            self.user_dict = yaml.full_load(f)
+        with open('data/bank.yaml', 'r', encoding="utf-8") as f:
+            self.bank_dict = yaml.full_load(f)
+
+    def register(self, user_id: int):
+        users = self.openFile('flow')
+        today = date.today()
+        users[user_id] = {'flow': 0, 'morning': today}
+        self.transaction(user_id, 20)
+        self.saveData(users, 'flow')
+
+    def transaction(self, user_id: int, flow_for_user: int):
+        users = self.openFile('flow')
+        bank = self.openFile('bank')
+        if user_id in users:
+            users[user_id]['flow'] += flow_for_user
+            bank['flow'] -= flow_for_user
+            print(log(True, False, 'Transaction',
+                  f'user({user_id}): {str(flow_for_user)}, bank: {str(flow_for_user)}'))
+        self.saveData(users, 'flow')
+        self.saveData(bank, 'bank')
+
+    def checkFlowAccount(self, user_id: int):
+        users = self.openFile('flow')
+        if user_id not in users:
+            self.register(user_id)
+            embed = errEmbed('找不到flow帳號!',
+                             f'<@{user_id}>\n現在申鶴已經創建了一個, 請重新執行操作')
+            return False, embed
+        else:
+            return True, None
+
+    def openFile(self, file_name:str):
+        with open(f'data/{file_name}.yaml', 'r', encoding="utf-8") as f:
+            return yaml.full_load(f)
+
+    def saveData(self, data: dict, file_name: str):
+        with open(f'data/{file_name}.yaml', 'w', encoding='utf-8') as f:
+            yaml.dump(data, f)
+
+flow_app = FlowApp()
