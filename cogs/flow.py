e16d557a4fc7d1af1a0cba794852b94d391e5adf
seria_ati <seria.ati@gmail.com>
2022-05-03 08:43:15 +0800

    use saveFile and openFile from utils

--- cogs/flow.py
+++ cogs/flow.py
@@ -7,7 +7,7 @@ from typing import List, Optional
 import uuid
 import random
 import yaml
-from utility.utils import defaultEmbed, errEmbed, log
+from utility.utils import defaultEmbed, errEmbed, log, openFile, saveFile
 import discord
 from utility.FlowApp import flow_app
 
@@ -22,8 +22,8 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
         if reactor.bot:
             return
 
-        users = flow_app.openFile('flow')
-        giveaways = flow_app.openFile('giveaways')
+        users = openFile('flow')
+        giveaways = openFile('giveaways')
         channel = self.bot.get_channel(payload.channel_id)
         discordID = payload.user_id
         guild = self.bot.get_guild(916838066117824553)
@@ -51,7 +51,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
                 discordID, -int(giveaways[payload.message_id]['ticket']))
             giveaways[payload.message_id]['current'] += giveaways[payload.message_id]['ticket']
             giveaways[payload.message_id]['members'].append(discordID)
-            flow_app.saveData(giveaways, 'giveaways')
+            saveFile(giveaways, 'giveaways')
             giveawayMsg = await channel.fetch_message(payload.message_id)
             newEmbed = defaultEmbed(
                 ":tada: 抽獎啦!!!",
@@ -72,7 +72,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
                 await channel.send(f"{lulurR.mention} {winnerUser.mention}")
                 await channel.send(embed=embed)
                 del giveaways[payload.message_id]
-                flow_app.saveData(giveaways, 'giveaways')
+                saveFile(giveaways, 'giveaways')
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
@@ -80,7 +80,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
         if reactor.bot:
             return
 
-        giveaways = flow_app.openFile('giveaways')
+        giveaways = openFile('giveaways')
         channel = self.bot.get_channel(payload.channel_id)
         discordID = payload.user_id
 
@@ -89,7 +89,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
                 giveaways[payload.message_id]['ticket']))
             giveaways[payload.message_id]['current'] -= giveaways[payload.message_id]['ticket']
             giveaways[payload.message_id]['members'].remove(discordID)
-            flow_app.saveData(giveaways, 'giveaways')
+            saveFile(giveaways, 'giveaways')
             giveawayMsg = await channel.fetch_message(payload.message_id)
             newEmbed = defaultEmbed(
                 ":tada: 抽獎啦!!!",
@@ -102,7 +102,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        users = flow_app.openFile('flow')
+        users = openFile('flow')
         discordID = message.author.id
         user = self.bot.get_user(message.author.id)
         if message.author.bot:
@@ -117,7 +117,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             elif users[discordID]['morning'] != today:
                 flow_app.transaction(discordID, 1)
                 users[discordID]['morning'] = today
-                flow_app.saveData(users, 'flow')
+                saveFile(users, 'flow')
                 await message.add_reaction(f"☀️")
 
     @app_commands.command(name='forceroll', description='強制抽出得獎者')
@@ -125,7 +125,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     @app_commands.checks.has_role('小雪團隊')
     async def forceroll(self, interaction: discord.Interaction, msgID: int):
         print(log(False, False, 'Forceroll', interaction.user.id))
-        giveaways = flow_app.openFile('giveaways')
+        giveaways = openFile('giveaways')
         giveawayMsg = self.bot.fetch_message(msgID)
         giveawayChannel = self.bot.get_channel(965517075508498452)
         lulurR = self.bot.get_user(665092644883398671)
@@ -141,7 +141,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             await giveawayChannel.send(f"{lulurR.mention} {winnerUser.mention}")
             await giveawayChannel.send(embed=embed)
             del giveaways[msgID]
-            flow_app.saveData(giveaways, 'giveaways')
+            saveFile(giveaways, 'giveaways')
             await interaction.response.send_message(f'{msgID} 強制抽獎成功', ephemeral=True)
 
     @forceroll.error
@@ -156,7 +156,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
                   member: Optional[Member] = None
                   ):
         print(log(False, False, 'Acc', interaction.user.id))
-        users = flow_app.openFile('flow')
+        users = openFile('flow')
         member = member or interaction.user
         discordID = member.id
         check, msg = flow_app.checkFlowAccount(discordID)
@@ -175,7 +175,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     async def give(self, interaction: discord.Interaction, member: Member, flow: int):
         print(log(False, False, 'Give',
               f'{interaction.user.id} give {flow} to {member.id}'))
-        users = flow_app.openFile('flow')
+        users = openFile('flow')
         if member.id == interaction.user.id:
             await interaction.response.send_message(
                 embed=errEmbed(
@@ -213,7 +213,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
         try:
             users[giverID]['flow'] -= int(flow)
             users[acceptorID]['flow'] += int(flow)
-            flow_app.saveData(users, 'flow')
+            saveFile(users, 'flow')
             embed = defaultEmbed(
                 "✅ 交易成功",
                 f"{self.bot.get_user(giverID).mention} • **-{flow}**\n"
@@ -275,8 +275,8 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     @app_commands.command(name='total', description='查看目前群組帳號及銀行flow幣分配情況')
     async def total(self, interaction: discord.Interaction):
         print(log(False, False, 'Total', f'{interaction.user.id}'))
-        users = flow_app.openFile('flow')
-        bank = flow_app.openFile('bank')
+        users = openFile('flow')
+        bank = openFile('bank')
         total = 0
         count = 0
         for user in users:
@@ -292,7 +292,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     @app_commands.command(name='flows', description='查看群組內所有flow帳號')
     async def flows(self, interaction: discord.Interaction):
         print(log(False, False, 'flows', interaction.user.id))
-        users = flow_app.openFile('flow')
+        users = openFile('flow')
         userStr = ""
         count = 1
         for user in users:
@@ -307,7 +307,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     @shop.command(name='show', description='顯示商店')
     async def show(self, interaction: discord.Interaction):
         print(log(False, False, 'shop show', interaction.user.id))
-        shop = flow_app.openFile('shop')
+        shop = openFile('shop')
         itemStr = ""
         for item, value in shop.items():
             itemStr = itemStr + \
@@ -321,12 +321,12 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     async def newitem(self, interaction: discord.Interaction, item: str, flow: int, max: int):
         print(log(False, False, 'shop newitem',
               f'{interaction.user.id}: (item={item}, flow={flow}, max={max})'))
-        shop = flow_app.openFile('shop')
+        shop = openFile('shop')
         uuid = str(uuid.uuid4())
         try:
             shop[item] = {'uuid': str(uuid), 'flow': int(
                 flow), 'current': 0, 'max': int(max)}
-            flow_app.saveData(shop, 'shop')
+            saveFile(shop, 'shop')
             await interaction.response.send_message(f"商品{item}新增成功")
         except Exception as e:
             print(log(True, True, 'shop newitem', e))
@@ -337,7 +337,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             await interaction.response.send_message('你不是小雪團隊的一員!', ephemeral=True)
 
     async def shop_autocomplete(self, interaction: discord.Interaction, current: str,) -> List[app_commands.Choice[str]]:
-        shop = flow_app.openFile('shop')
+        shop = openFile('shop')
         return [
             app_commands.Choice(name=shop, value=shop)
             for shop in shop if current.lower() in shop.lower()
@@ -351,14 +351,14 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     async def removeitem(self, interaction: discord.Interaction, item: str):
         print(log(False, False, 'shop removeitem',
               f'{interaction.user.id}: (item={item})'))
-        shop = flow_app.openFile('shop')
+        shop = openFile('shop')
         if item not in shop:
             embed = errEmbed('找不到該商品!', '')
             await interaction.response.send_message(embed=embed, ephemeral=True)
         else:
             try:
                 del shop[item]
-                flow_app.saveData(shop, 'shop')
+                saveFile(shop, 'shop')
                 await interaction.response.send_message("商品刪除成功")
             except Exception as e:
                 print(log(True, True, 'shop removeitem', e))
@@ -375,9 +375,9 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     async def buy(self, interaction: discord.Interaction, item: str):
         print(log(False, False, 'shop buy',
               f'{interaction.user.id}: (item={item})'))
-        users = flow_app.openFile('flow')
-        shop = flow_app.openFile('shop')
-        logs = flow_app.openFile('log')
+        users = openFile('flow')
+        shop = openFile('shop')
+        logs = openFile('log')
         discordID = interaction.user.id
         check, msg = flow_app.checkFlowAccount(discordID)
         if check == False:
@@ -397,7 +397,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
         logID = str(uuid.uuid4())
         logs[logID] = {'item': item,
                        'flow': itemPrice, 'buyerID': interaction.user.id}
-        flow_app.saveData(logs, 'log')
+        saveFile(logs, 'log')
         flow_app.transaction(discordID, -int(itemPrice))
         await interaction.response.send_message(f"商品 {item} 購買成功, 詳情請查看私訊")
         await interaction.user.send(f"您已在flow商城購買了 {item} 商品, 請將下方的收據截圖並寄給小雪或律律來兌換商品")
@@ -415,7 +415,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     async def shop_log(self, interaction: discord.Interaction):
         print(log(False, False, 'shop log', interaction.user.id))
         await interaction.response.send_message('購買紀錄如下', ephemeral=True)
-        logs = flow_app.openFile('log')
+        logs = openFile('log')
         for log in logs:
             logID = log
             user = self.bot.get_user(logs[logID]['buyerID'])
@@ -439,13 +439,13 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
     @app_commands.autocomplete(item=shop_autocomplete)
     async def clear(self, interaction: discord.Interaction, item: str):
         print(log(False, False, 'shop clear', interaction.user.id))
-        shop = flow_app.openFile('shop')
+        shop = openFile('shop')
         if item not in shop:
             await interaction.response.send_message(embed=errEmbed('找不到該商品!', ''), ephemeral=True)
         else:
             try:
                 shop[item]['current'] = 0
-                flow_app.saveData(shop, 'shop')
+                saveFile(shop, 'shop')
                 await interaction.response.send_message('已將所有商品的購買次數清零')
             except Exception as e:
                 print(log(True, True, 'shop clear', e))
@@ -463,7 +463,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             return True, f'成功'
 
     def check_flow(self, user_id: int, flow: int):
-        users = flow_app.openFile('flow')
+        users = openFile('flow')
         if int(flow) < 0:
             result = errEmbed("發布失敗, 請輸入大於1的flow幣", "")
             return False, result
@@ -488,8 +488,8 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             @discord.ui.button(label='OK', style=discord.ButtonStyle.blurple)
             async def ok_confirm(self, interaction: discord.Interaction, button: discord.ui.button):
                 msg = interaction.message
-                confirms = flow_app.openFile('confirm')
-                users = flow_app.openFile('flow')
+                confirms = openFile('confirm')
+                users = openFile('flow')
                 authorID = confirms[msg.id]['authorID']
                 receiverID = confirms[msg.id]['receiverID']
                 flow = confirms[msg.id]['flow']
@@ -517,16 +517,16 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
                 await author.send(embed=embed)
                 await receiver.send(embed=embed)
                 del confirms[msg.id]
-                FlowCog.saveData(confirms, 'confirm')
-                FlowCog.saveData(users, 'flow')
+                saveFile(confirms, 'confirm')
+                saveFile(users, 'flow')
 
         @discord.ui.button(label='接受委託', style=discord.ButtonStyle.green)
         async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
             msg = interaction.message
             with open('data/find.yaml', 'r', encoding="utf-8") as f:
                 finds = yaml.full_load(f)
-            users = flow_app.openFile('flow')
-            confirms = flow_app.openFile('confirm')
+            users = openFile('flow')
+            confirms = openFile('confirm')
             if msg.id in finds:
                 self.stop()
                 author = interaction.client.get_user(finds[msg.id]['authorID'])
@@ -563,8 +563,8 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
                 confirms[dm.id] = {'title': finds[msg.id]['title'], 'authorID': int(
                     finds[msg.id]['authorID']), 'receiverID': interaction.user.id, 'flow': finds[msg.id]['flow'], 'type': finds[msg.id]['type']}
                 del finds[msg.id]
-                FlowCog.saveData(self, finds, 'find')
-                FlowCog.saveData(self, confirms, 'confirm')
+                saveFile(finds, 'find')
+                saveFile(confirms, 'confirm')
 
     @app_commands.command(name='find', description='發布委託')
     @app_commands.rename(type='委託類型', title='幫助名稱', flow='flow幣數量')
@@ -634,10 +634,10 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
         role = guild.get_role(965141973700857876) #委託通知
         await interaction.channel.send(role.mention)
         msg = await interaction.original_message()
-        finds = flow_app.openFile('find')
+        finds = openFile('find')
         finds[msg.id] = {'title': title, 'flow': int(flow),
                          'author': str(interaction.user), 'authorID': interaction.user.id, 'type': 1}
-        flow_app.saveData(finds, 'find')
+        saveFile(finds, 'find')
         await view.wait()
 
     @app_commands.command(name='giveaway', description='設置抽獎')
@@ -652,7 +652,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             prize: str, goal: int, ticket: int, role: Optional[Role] = None):
         print(log(False, False, 'giveaway',
               f'{interaction.user.id}: (prize={prize}, goal={goal}, ticket={ticket}, role={role})'))
-        giveaways = flow_app.openFile('giveaways')
+        giveaways = openFile('giveaways')
         if role is not None:
             embedGiveaway = defaultEmbed(
                 ":tada: 抽獎啦!!!",
@@ -689,7 +689,7 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             'members': [],
             'role': role.id
         }
-        flow_app.saveData(giveaways, 'giveaways')
+        saveFile(giveaways, 'giveaways')
 
     @giveaway.error
     async def err_handle(self, interaction: discord.Interaction, e: app_commands.AppCommandError):
