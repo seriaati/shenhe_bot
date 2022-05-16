d49b2f2cbffe05ef565e5fcb6da024532e68588d
seria_ati <seria.ati@gmail.com>
2022-05-16 09:03:53 +0800

    清理

--- utility/utils.py
+++ utility/utils.py
@@ -5,24 +5,15 @@ import yaml
 from utility.character_name import character_names
 
 
-global warningColor, purpleColor, footerAuthor, footerImage, timeOutErrorMsg, embedNoAccount, embedNoGroup, groups, whyRegister
-warningColor = 0xfc5165
-purpleColor = 0xa68bd3
-footerAuthor = "輸入!help來獲得幫助"
-footerImage = "https://i.imgur.com/DWYpYrd.jpg"
-timeOutErrorMsg = "已取消當前操作, 請在30秒內回答問題"
-embedNoAccount = discord.Embed(
-    title="😢 該帳號不存在", description="請使用`!register`來註冊帳號, 如有疑問請@小雪", color=warningColor)
-whyRegister = "• `!abyss`炫耀你的深淵傷害(或被嘲笑)\n• `!check`查看目前的派遣、樹脂、塵歌壺等狀況\n• `!char`炫耀你擁有的角色\n• `!stats`證明你是大佬\n• `!diary`看看這個月要不要課\n• `!area`炫耀探索度(或被嘲笑)\n• `!today`今天的肝還在嗎\n• 自動領取hoyolab網頁登入獎勵\n• 樹脂提醒功能(詳情請打`!dm`)"
+def defaultEmbed(title: str, message: str):
+    return discord.Embed(title=title, description=message, color=0xa68bd3)
 
 
-def defaultEmbed(title:str, message:str):
-    return discord.Embed(title=title, description=message, color=purpleColor)
+def errEmbed(title: str, message: str):
+    return discord.Embed(title=title, description=message, color=0xfc5165)
 
-def errEmbed(title:str, message:str):
-    return discord.Embed(title=title, description=message, color=warningColor)
 
-def log(is_system:bool, is_error:bool, log_type:str, log_msg:str):
+def log(is_system: bool, is_error: bool, log_type: str, log_msg: str):
     system = "SYSTEM"
     if not is_system:
         system = "USER"
@@ -31,10 +22,12 @@ def log(is_system:bool, is_error:bool, log_type:str, log_msg:str):
     else:
         result = f"[{system}][ERROR][{log_type}] {log_msg}"
     return result
-    
+
+
 def getCharacterName(character: genshin.models.BaseCharacter) -> str:
     chinese_name = character_names.get(character.id)
     return chinese_name if chinese_name != None else character.name
+
 
 def trimCookie(cookie: str) -> str:
     try:
@@ -48,17 +41,22 @@ def trimCookie(cookie: str) -> str:
         new_cookie = None
     return new_cookie
 
+
 weekday_dict = {0: '週一', 1: '週二', 2: '週三', 3: '週四', 4: '週五', 5: '週六', 6: '週日'}
+
+
 def getWeekdayName(n: int) -> str:
     return weekday_dict.get(n)
 
-def openFile(file_name:str) -> dict:
+
+def openFile(file_name: str) -> dict:
     with open(f'data/{file_name}.yaml', 'r', encoding='utf-8') as file:
-        result =  yaml.unsafe_load(file)
+        result = yaml.unsafe_load(file)
     if result is None:
         result = {}
     return result
 
-def saveFile(data:dict, file_name:str):
+
+def saveFile(data: dict, file_name: str):
     with open(f'data/{file_name}.yaml', 'w', encoding='utf-8') as f:
         yaml.dump(data, f)
