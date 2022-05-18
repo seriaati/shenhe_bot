bb6c969c594368b23b174426b51fb90eb7c0f297
seria_ati <seria.ati@gmail.com>
2022-05-18 08:41:54 +0800

    errEmbed 優化

--- utility/utils.py
+++ utility/utils.py
@@ -5,15 +5,15 @@ import yaml
 from utility.character_name import character_names
 
 
-def defaultEmbed(title: str, message: str=''):
+def defaultEmbed(title: str, message: str = ''):
     return discord.Embed(title=title, description=message, color=0xa68bd3)
 
 
-def ayaakaaEmbed(title: str, message: str=''):
+def ayaakaaEmbed(title: str, message: str = ''):
     return discord.Embed(title=title, description=message, color=0xCBD9EB)
 
 
-def errEmbed(title: str, message: str):
+def errEmbed(title: str, message: str = ''):
     return discord.Embed(title=title, description=message, color=0xfc5165)
 
 
