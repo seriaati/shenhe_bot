075eb4b41e5a0163c5d6e8d8f94fbe1fd608aa40
seria_ati <seria.ati@gmail.com>
2022-05-19 18:06:25 +0800

    giveaway 配合 persistent view 的更新

--- cogs/gvaway.py
+++ cogs/gvaway.py
@@ -1,31 +1,22 @@
 import random
-from typing import List, Optional
+from typing import Optional
 import discord
+from discord import Message, SelectOption
 from discord.ext import commands
 from discord import Interaction, Role, app_commands
 from discord.app_commands import Choice
+from discord.ui import Select, View
 from utility.FlowApp import flow_app
 from utility.utils import defaultEmbed, errEmbed, log, openFile, saveFile
+
+global gv_channel_id, debug_toggle
+debug_toggle = False
+gv_channel_id = 965517075508498452 if not debug_toggle else 909595117952856084
 
 
 class GiveAwayCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-
-    def generate_gv_embed(self, gv_msg_id: int, i:Interaction):
-        gv = openFile('giveaways')
-        role = i.guild.get_role(gv[gv_msg_id]['role'])
-        role_exclusive = f'此抽獎專屬於: {role.mention}成員' if role is not None else '任何人都可以參加這個抽獎'
-        refund_state = '(會退款)' if gv[gv_msg_id]['refund_mode'] == True else '(不會退款)'
-        embed = defaultEmbed(
-            ":tada: 抽獎啦!!!",
-            f"獎品: {gv[gv_msg_id]['prize']}\n"
-            f"目前flow幣: {gv[gv_msg_id]['current']}/{gv[gv_msg_id]['goal']}\n"
-            f"參加抽獎要付的flow幣: {gv[gv_msg_id]['ticket']}\n"
-            f"{role_exclusive}\n"
-            f'{refund_state}\n'
-            f"輸入`/join`指令來參加抽獎")
-        return embed
 
     @app_commands.command(name='giveaway', description='設置抽獎')
     @app_commands.checks.has_role('小雪團隊')
@@ -40,23 +31,23 @@ class GiveAwayCog(commands.Cog):
         Choice(name='是', value=0),
         Choice(name='否', value=1)
     ])
-    async def giveaway(
-            self, interaction: discord.Interaction,
+    async def create_giveaway(
+            self, interaction: Interaction,
             prize: str, goal: int, ticket: int, role: Optional[Role] = None, refund_mode: int = 1):
         print(log(False, False, 'giveaway',
               f'{interaction.user.id}: (prize={prize}, goal={goal}, ticket={ticket}, role={role}, refund_mode={refund_mode})'))
-        channel = interaction.client.get_channel(965517075508498452)  # 抽獎台
-        # channel = interaction.client.get_channel(909595117952856084) #測試抽獎台
+        channel = interaction.client.get_channel(gv_channel_id)
+        role = role or interaction.guild.get_role(967035645610573834)
         role_exclusive = f'此抽獎專屬於: {role.mention}成員' if role is not None else '任何人都可以參加這個抽獎'
         refund_state = '(會退款)' if refund_mode == 0 else '(不會退款)'
-        await interaction.response.send_message(embed = defaultEmbed(
+        giveaway_view = GiveAwayCog.GiveAwayView(interaction)
+        await interaction.response.send_message(embed=defaultEmbed(
             ":tada: 抽獎啦!!!",
             f"獎品: {prize}\n"
             f"目前flow幣: 0/{goal}\n"
             f"參加抽獎要付的flow幣: {ticket}\n"
             f"{role_exclusive}\n"
-            f'{refund_state}\n'
-            f"輸入`/join`指令來參加抽獎"))
+            f'{refund_state}'), view=giveaway_view)
         refund_mode_toggle = True if refund_mode == 0 else False
         msg = await interaction.original_message()
         gv = openFile('giveaways')
@@ -76,16 +67,29 @@ class GiveAwayCog(commands.Cog):
         else:
             await channel.send(role.mention)
 
-    @giveaway.error
-    async def err_handle(self, interaction: discord.Interaction, e: app_commands.AppCommandError):
+    @create_giveaway.error
+    async def err_handle(self, interaction: Interaction, e: app_commands.AppCommandError):
         if isinstance(e, app_commands.errors.MissingRole):
             await interaction.response.send_message('你不是小雪團隊的一員!', ephemeral=True)
 
-    class GiveAwayView(discord.ui.View):
-        def __init__(self, interaction: discord.Interaction, gv_msg_id: int):
+    class GiveAwayView(View):
+        def __init__(self, interaction: Interaction = None):
             super().__init__(timeout=None)
             self.interaction = interaction
-            self.msg_id = gv_msg_id
+
+        def generate_gv_embed(self, gv_msg_id: int, interaction: Interaction):
+            gv = openFile('giveaways')
+            role = interaction.guild.get_role(gv[gv_msg_id]['role'])
+            role_exclusive = f'此抽獎專屬於: {role.mention}成員' if role is not None else '任何人都可以參加這個抽獎'
+            refund_state = '(會退款)' if gv[gv_msg_id]['refund_mode'] == True else '(不會退款)'
+            embed = defaultEmbed(
+                ":tada: 抽獎啦!!!",
+                f"獎品: {gv[gv_msg_id]['prize']}\n"
+                f"目前flow幣: {gv[gv_msg_id]['current']}/{gv[gv_msg_id]['goal']}\n"
+                f"參加抽獎要付的flow幣: {gv[gv_msg_id]['ticket']}\n"
+                f"{role_exclusive}\n"
+                f'{refund_state}')
+            return embed
 
         def ticket_flow_check(self, user_id: int, ticket: int):
             users = openFile('flow')
@@ -108,28 +112,30 @@ class GiveAwayCog(commands.Cog):
                 gv[gv_msg_id]['members'].append(user_id)
             saveFile(gv, 'giveaways')
 
-        async def update_gv_msg(self, input_gv_msg_id: int):
-            channel = self.interaction.client.get_channel(965517075508498452) #抽獎台
-            # channel = self.interaction.client.get_channel(909595117952856084) #測試抽獎台
-            gv_msg = await channel.fetch_message(input_gv_msg_id)
-            await gv_msg.edit(embed=GiveAwayCog.generate_gv_embed(self, gv_msg_id=input_gv_msg_id, i=self.interaction))
+        async def update_gv_msg(self, gv_msg_id: int, interaction: Interaction):
+            channel = interaction.client.get_channel(gv_channel_id)
+            gv_msg = await channel.fetch_message(gv_msg_id)
+            await gv_msg.edit(embed=GiveAwayCog.GiveAwayView.generate_gv_embed(self, gv_msg_id, interaction))
 
         async def check_gv_finish(self, gv_msg_id: int, i: Interaction):
             gv = openFile('giveaways')
             if gv[gv_msg_id]['current'] == gv[gv_msg_id]['goal']:
-                channel = i.client.get_channel(965517075508498452)
+                channel = i.client.get_channel(gv_channel_id)
                 lulurR = i.client.get_user(665092644883398671)
                 winner_id = random.choice(gv[gv_msg_id]['members'])
                 winner = i.client.get_user(int(winner_id))
+                original_gv_msg: Message = await channel.fetch_message(gv_msg_id)
+                await original_gv_msg.delete()
                 embed = defaultEmbed(
                     "🎉 抽獎結果",
                     f"恭喜{winner.mention}獲得價值{gv[gv_msg_id]['goal']} flow幣的 {gv[gv_msg_id]['prize']} !")
                 await channel.send(f"{lulurR.mention} {winner.mention}")
                 await channel.send(embed=embed)
-                if gv[gv_msg_id]['refund_mode'] == True: #進行退款
+                if gv[gv_msg_id]['refund_mode'] == True:  # 進行退款
                     for user_id in gv[gv_msg_id]['members']:
-                        if winner_id!=user_id: #如果該ID不是得獎者
-                            flow_app.transaction(user_id, int(gv[gv_msg_id]['ticket'])/2) #退款入場費/2
+                        if winner_id != user_id:  # 如果該ID不是得獎者
+                            flow_app.transaction(user_id, int(
+                                gv[gv_msg_id]['ticket'])/2)  # 退款入場費/2
                 print(log(True, False, 'Giveaway Ended',
                       f'(gv_msg_id={gv_msg_id}, winner={winner_id})'))
                 del gv[gv_msg_id]
@@ -144,139 +150,91 @@ class GiveAwayCog(commands.Cog):
                 embed = errEmbed('你沒有參加過這個抽獎', '')
                 return False, embed
 
-        @discord.ui.button(label='參加抽獎',
-                           style=discord.ButtonStyle.green)
-        async def participate(self, interaction: discord.Interaction,
-                              button: discord.ui.Button):
-            msg_id = self.msg_id
+        @discord.ui.button(label='參加抽獎', style=discord.ButtonStyle.green, custom_id='join_give_away_button')
+        async def join_giveaway_callback(self, interaction: Interaction, button: discord.ui.Button):
+            msg = interaction.message
             check, check_msg = flow_app.checkFlowAccount(interaction.user.id)
             if check == False:
                 await interaction.response.send_message(embed=check_msg, ephemeral=True)
                 return
             gv = openFile('giveaways')
-            ticket = gv[msg_id]['ticket']
+            ticket = gv[msg.id]['ticket']
             check, check_msg = self.ticket_flow_check(
                 interaction.user.id, ticket)
             if check == False:
                 await interaction.response.send_message(embed=check_msg, ephemeral=True)
                 return
             check, check_msg = self.check_if_already_in_gv(
-                interaction.user.id, msg_id)
+                interaction.user.id, msg.id)
             if check == True:
                 await interaction.response.send_message(embed=check_msg, ephemeral=True)
                 return
-            if msg_id in gv:
-                r = interaction.guild.get_role(gv[msg_id]['role'])
+            if msg.id in gv:
+                r = interaction.guild.get_role(gv[msg.id]['role'])
                 if r is not None and r not in interaction.user.roles:
                     await interaction.response.send_message(embed=errEmbed(
                         '非常抱歉', f'你不是{r.mention}的一員, 不能參加這個抽獎'), ephemeral=True)
                     return
-                self.join_giveaway(interaction.user.id, ticket, msg_id)
-                await interaction.response.send_message(embed=defaultEmbed(f'✅ 參加抽獎成功', f'flow幣 -{ticket}'), ephemeral=True)
-                await interaction.followup.send(embed=GiveAwayCog.generate_gv_embed(self, gv_msg_id=msg_id, i=interaction), ephemeral=True)
-                await self.update_gv_msg(msg_id)
-                await self.check_gv_finish(gv_msg_id=msg_id, i=interaction)
+                self.join_giveaway(interaction.user.id, ticket, msg.id)
+                await interaction.response.send_message(embed=defaultEmbed(f'✅ 參加抽獎成功', f'你的flow幣 -{ticket}'), ephemeral=True)
+                await self.update_gv_msg(msg.id, interaction)
+                await self.check_gv_finish(gv_msg_id=msg.id, i=interaction)
             else:
                 await interaction.response.send_message(embed=errEmbed('該抽獎不存在!', '(因為某些不明原因)'))
 
-        @discord.ui.button(label='退出抽獎',
-                           style=discord.ButtonStyle.grey)
-        async def quit(self, interaction: discord.Interaction,
-                       button: discord.ui.Button):
-            msg_id = self.msg_id
+        @discord.ui.button(label='退出抽獎', style=discord.ButtonStyle.grey, custom_id='leave_giveaway_button')
+        async def leave_giveaway_callback(self, interaction: Interaction, button: discord.ui.Button):
+            msg = interaction.message
             gv = openFile('giveaways')
-            if msg_id in gv:
-                ticket = -int(gv[msg_id]['ticket'])
+            if msg.id in gv:
+                ticket = -int(gv[msg.id]['ticket'])
                 check, check_msg = self.check_if_already_in_gv(
-                    interaction.user.id, msg_id)
+                    interaction.user.id, msg.id)
                 if check == False:
                     await interaction.response.send_message(embed=check_msg, ephemeral=True)
                     return
-                self.join_giveaway(interaction.user.id, ticket, msg_id)
-                await interaction.response.send_message(embed=defaultEmbed(f'✅退出抽獎成功', f'flow幣 +{-int(ticket)}'), ephemeral=True)
-                await interaction.followup.send(embed=GiveAwayCog.generate_gv_embed(self, msg_id, interaction), ephemeral=True)
-                await self.update_gv_msg(msg_id)
+                self.join_giveaway(interaction.user.id, ticket, msg.id)
+                await interaction.response.send_message(embed=defaultEmbed(f'✅ 退出抽獎成功', f'你的flow幣 +{-int(ticket)}'), ephemeral=True)
+                await self.update_gv_msg(msg.id, interaction)
 
-    def is_gv_option_valid(self, gv_option: str):
-        if gv_option == '目前沒有任何進行中的抽獎':
-            return False, errEmbed('真的沒有抽獎', '真的')
-        gv = openFile('giveaways')
-        found = False
-        for gv_id, val in gv.items():
-            if val['prize']==gv_option:
-                found = True
-        if found == False:
-            return False, errEmbed('該抽獎不存在!', '')
-        return True, None
+    class GiveawayDropdown(Select):
+        def __init__(self, gv_dict: dict):
+            options = []
+            if not bool(gv_dict):
+                super().__init__(placeholder='目前沒有進行中的抽獎', min_values=1, max_values=1,
+                                 options=[SelectOption(label='disabled')], disabled=True)
+            else:
+                for msg_id, val in gv_dict.items():
+                    options.append(SelectOption(
+                        label=val['prize'], value=msg_id))
+                super().__init__(placeholder='選擇要結束的抽獎', min_values=1, max_values=1, options=options)
 
-    async def giveaway_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice[str]]:
-        gv = openFile('giveaways')
-        if not gv:
-            result = ['目前沒有任何進行中的抽獎']
-        else:
-            result = []
-            for msg_id, val in gv.items():
-                result.append(val['prize'])
-        return [
-            app_commands.Choice(name=gv, value=gv)
-            for gv in result if current.lower() in gv.lower()
-        ]
+        async def callback(self, i: Interaction):
+            print(log(False, False, 'End Giveaway',
+                  f'{i.user.id}: (gv_msg_id = {self.values[0]})'))
+            gv = openFile('giveaways')
+            gv_msg_id = int(self.values[0])
+            gv[gv_msg_id]['current'] = gv[gv_msg_id]['goal']
+            saveFile(gv, 'giveaways')
+            await GiveAwayCog.GiveAwayView.check_gv_finish(self, gv_msg_id=gv_msg_id, i=i)
+            await i.response.send_message(embed=defaultEmbed('✅ 強制抽獎成功'), ephemeral=True)
 
-    @app_commands.command(name='join', description='參加抽獎')
-    @app_commands.rename(gv_option='抽獎')
-    @app_commands.describe(gv_option='請選擇想要參與的抽獎')
-    @app_commands.autocomplete(gv_option=giveaway_autocomplete)
-    async def join_giveaway(self, i: Interaction, gv_option: str):
-        print(log(False, False, 'Join Giveaway', i.user.id))
-        check, check_msg = self.is_gv_option_valid(gv_option)
-        if check == False:
-            await i.response.send_message(embed=check_msg, ephemeral=True)
-            return
-        gv = openFile('giveaways')
-        for msg_id, value in gv.items():
-            if value['prize'] == gv_option:
-                gv_msg_id = msg_id
-        view = self.GiveAwayView(interaction=i, gv_msg_id=gv_msg_id)
-        await i.response.send_message(embed=self.generate_gv_embed(gv_msg_id=gv_msg_id, i=i), view=view, ephemeral=True)
+    class GiveawayDropdownView(View):
+        def __init__(self, gv_dict: dict):
+            super().__init__(timeout=None)
+            self.add_item(GiveAwayCog.GiveawayDropdown(gv_dict))
 
     @app_commands.command(name='endgiveaway', description='強制結束抽獎並選出得獎者')
-    @app_commands.rename(gv_option='抽獎')
-    @app_commands.describe(gv_option='請選擇想要參與的抽獎')
-    @app_commands.autocomplete(gv_option=giveaway_autocomplete)
     @app_commands.checks.has_role('小雪團隊')
-    async def end_giveaway(self, interaction: discord.Interaction, gv_option: str):
-        print(log(False, False, 'End Giveaway', interaction.user.id))
-        check, check_msg = self.is_gv_option_valid(gv_option)
-        if check == False:
-            await interaction.response.send_message(embed=check_msg, ephemeral=True)
+    async def end_giveaway(self, interaction: Interaction):
         gv = openFile('giveaways')
-        for msg_id, value in gv.items():
-            if value['prize'] == gv_option:
-                gv_msg_id = msg_id
-                break
-        gv[msg_id]['current'] = gv[msg_id]['goal']
-        saveFile(gv, 'giveaways')
-        GiveAwayCog.GiveAwayView.check_gv_finish(gv_msg_id=msg_id, i=interaction)
-        await interaction.response.send_message(embed=defaultEmbed('✅強制抽獎成功', f'獎品: {gv_option}'), ephemeral=True)
+        view = self.GiveawayDropdownView(gv)
+        await interaction.response.send_message(view=view, ephemeral=True)
 
     @end_giveaway.error
-    async def err_handle(self, interaction: discord.Interaction, e: app_commands.AppCommandError):
+    async def err_handle(self, interaction: Interaction, e: app_commands.AppCommandError):
         if isinstance(e, app_commands.errors.MissingRole):
             await interaction.response.send_message('你不是小雪團隊的一員!', ephemeral=True)
-
-    @end_giveaway.autocomplete('gv_option')
-    async def giveaway_autocomplete(self, interaction: discord.Interaction, current: str) -> List[app_commands.Choice[str]]:
-        gv = openFile('giveaways')
-        if not gv:
-            result = ['目前沒有任何進行中的抽獎']
-        else:
-            result = []
-            for msg_id, val in gv.items():
-                result.append(val['prize'])
-        return [
-            app_commands.Choice(name=gv, value=gv)
-            for gv in result if current.lower() in gv.lower()
-        ]
 
 
 async def setup(bot: commands.Bot) -> None:
