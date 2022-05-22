7bcf398cff3e9b4820611eb7fcb762364cff9cdb
Ayaakaa <80079457+Ayaakaa@users.noreply.github.com>
2022-05-22 14:17:24 +0800

    改了 /give 的一點東西，不知道有沒有用，不過不妨一試

--- cogs/flow.py
+++ cogs/flow.py
@@ -178,8 +178,8 @@ class FlowCog(commands.Cog, name='flow', description='flow系統相關'):
             await interaction.response.send_message(embed=embed, ephemeral=True)
             return
         try:
-            flow_app.transaction(giverID, -int(flow))
-            flow_app.transaction(acceptorID, int(flow))
+            flow_app.transaction(int(giverID), -int(flow))
+            flow_app.transaction(int(acceptorID), int(flow))
             saveFile(users, 'flow')
             embed = defaultEmbed(
                 "✅ 交易成功",
