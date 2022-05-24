45f424753b17ed33f4d3b59ba646e74cba194b8f
seria_ati <seria.ati@gmail.com>
2022-05-24 13:52:40 +0800

    移除openfile及savefile

--- utility/utils.py
+++ utility/utils.py
@@ -57,18 +57,3 @@ weekday_dict = {0: '週一', 1: '週二', 2: '週三', 3: '週四', 4: '週五',
 
 def getWeekdayName(n: int) -> str:
     return weekday_dict.get(n)
-
-
-def openFile(file_name: str) -> dict:
-    if not os.path.exists(f'data/{file_name}.yaml'):
-        f = open(f'data/{file_name}.yaml', 'w+')
-    with open(f'data/{file_name}.yaml', 'r', encoding='utf-8') as file:
-        result = yaml.unsafe_load(file)
-    if result is None:
-        result = {}
-    return result
-
-
-def saveFile(data: dict, file_name: str):
-    with open(f'data/{file_name}.yaml', 'w', encoding='utf-8') as f:
-        yaml.dump(data, f)
