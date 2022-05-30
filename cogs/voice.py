2948498abc76862fb211c835e9c70fbf148c0314
seria_ati <seria.ati@gmail.com>
2022-05-30 18:20:21 +0800

    修復了可以無限複製語音台的bug

--- cogs/voice.py
+++ cogs/voice.py
@@ -8,13 +8,17 @@ class VoiceChannel(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
-        vc: VoiceChannel = self.bot.get_channel(980771791406501898)
+        vc: VoiceChannel = self.bot.get_channel(980772222148952064) if not self.bot.debug_toggle else self.bot.get_channel(980775512437837834)
+        vc_role = member.guild.get_role(980774103344640000) if not self.bot.debug_toggle else member.guild.get_role(980774369771008051)
         old_channel: VoiceChannel = before.channel
         new_channel: VoiceChannel = after.channel
         if new_channel == vc:
             member_vc = await member.guild.create_voice_channel(name=f'{member.name}的語音台', category=vc.category)
             await member.move_to(member_vc)
-        if len(old_channel.members) == 0: 
+            await member.add_roles(vc_role)
+        if new_channel is None:
+            await member.remove_roles(vc_role)
+        if (new_channel is None or new_channel == vc) and old_channel is not None and len(old_channel.members) == 0:
             await old_channel.delete()
 
 
