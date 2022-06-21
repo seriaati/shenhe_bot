b1a1480c6dc1c6bb8ef8ff57ebf3ed0b9364a095
seriaati <seria.ati@email.com>
2022-06-21 15:10:06 +0800

    messaage delete listener

--- cogs/sese.py
+++ cogs/sese.py
@@ -1,7 +1,8 @@
-from discord import Message
+from datetime import datetime
+from discord import Message, RawMessageDeleteEvent
 from discord.ext import commands
 
-from utility.utils import errEmbed
+from utility.utils import defaultEmbed, errEmbed
 class SeseCog(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
@@ -17,6 +18,21 @@ class SeseCog(commands.Cog):
                 if not attachment.is_spoiler():
                     await message.delete()
                     await message.channel.send(embed=errEmbed('在色色台發圖片請spoiler!'), delete_after=3)
+
+    @commands.Cog.listener()
+    async def on_raw_message_delete(self, payload: RawMessageDeleteEvent):
+        if payload.cached_message is not None:
+            embed = defaultEmbed(
+                '偵測到訊息刪除',
+                f'「{payload.cached_message.content}」\n\n'
+                f'用戶: {payload.cached_message.author.mention}\n'
+                f'頻道: {payload.cached_message.channel.mention}\n'
+                f'時間: {datetime.now().strftime("%m/%d/%Y %H:%M:%S")}'
+            )
+            embed.set_footer(text=f'用戶 ID: {payload.cached_message.author.id}\n')
+            embed.set_author(name=payload.cached_message.author, icon_url=payload.cached_message.author.avatar)
+            c = self.bot.get_channel(988698669442269184) if not self.bot.debug_toggle else self.bot.get_channel(909595117952856084)
+            await c.send(embed=embed)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(SeseCog(bot))
