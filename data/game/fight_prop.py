c5489bda2cf03b6de593f7229f44b5b391989e9e
seriaati <seria.ati@email.com>
2022-07-15 12:48:39 +0800

    fight prop 資料更新

--- data/game/fight_prop.py
+++ data/game/fight_prop.py
@@ -1,35 +1,122 @@
-fight_prop_name = {
-	'FIGHT_PROP_BASE_HP': '基礎生命值',
-	'FIGHT_PROP_HP': '生命值',
-	'FIGHT_PROP_HP_PERCENT': '生命值%',
-	'FIGHT_PROP_BASE_ATTACK': '基礎攻擊力',
-	'FIGHT_PROP_ATTACK': '攻擊力',
-	'FIGHT_PROP_ATTACK_PERCENT': '攻擊力%',
-	'FIGHT_PROP_BASE_DEFENSE': '基礎防禦力',
-	'FIGHT_PROP_DEFENSE': '防禦力',
-	'FIGHT_PROP_DEFENSE_PERCENT': '防禦力%',
-	'FIGHT_PROP_BASE_SPEED': '基礎移動速度',
-	'FIGHT_PROP_SPEED_PERCENT': '移動速度%',
-	'FIGHT_PROP_HP_MP_PERCENT': None,
-	'FIGHT_PROP_ATTACK_MP_PERCENT': None,
-	'FIGHT_PROP_CRITICAL': '暴擊率%',
-	'FIGHT_PROP_ANTI_CRITICAL': None,
-	'FIGHT_PROP_CRITICAL_HURT': '暴擊傷害%',
-	'FIGHT_PROP_CHARGE_EFFICIENCY': '元素充能%',
-	'FIGHT_PROP_ADD_HURT': None,
-	'FIGHT_PROP_SUB_HURT': None,
-	'FIGHT_PROP_HEAL_ADD': '治療加成%',
-	'FIGHT_PROP_HEALED_ADD': None,
-	'FIGHT_PROP_ELEMENT_MASTERY': '元素精通',
-	'FIGHT_PROP_PHYSICAL_SUB_HURT': None,
-	'FIGHT_PROP_PHYSICAL_ADD_HURT': '物傷加成%',
-	'FIGHT_PROP_DEFENCE_IGNORE_RATIO': None,
-	'FIGHT_PROP_DEFENCE_IGNORE_DELTA': None,
-	'FIGHT_PROP_FIRE_ADD_HURT': '火傷加成%',
-	'FIGHT_PROP_ELEC_ADD_HURT': '雷傷加成%',
-	'FIGHT_PROP_WATER_ADD_HURT': '水傷加成%',
-	'FIGHT_PROP_GRASS_ADD_HURT': '草傷加成%',
-	'FIGHT_PROP_WIND_ADD_HURT': '風傷加成%',
-	'FIGHT_PROP_ROCK_ADD_HURT': '岩傷加成%',
-	'FIGHT_PROP_ICE_ADD_HURT': '冰傷加成%',
+fight_prop = {
+    "FIGHT_PROP_HP": {
+        "name": "生命值",
+        "emoji": "<:HP:982068466410463272>",
+        "substat": True
+    },
+    "FIGHT_PROP_HP_PERCENT": {
+        "name": "生命值%",
+        "emoji": "<:HP_PERCENT:982138227450347553>",
+        "substat": True
+    },
+    "FIGHT_PROP_ATTACK": {
+        "name": "攻擊力",
+        "emoji": "<:ATTACK:982138214305390632>",
+        "substat": True
+    },
+    "FIGHT_PROP_ATTACK_PERCENT": {
+        "name": "攻擊力%",
+        "emoji": "<:ATTACK_PERCENT:982138215832117308>",
+        "substat": True
+    },
+    "FIGHT_PROP_DEFENSE": {
+        "name": "防禦力",
+        "emoji": "<:DEFENSE:982068463566721064>",
+        "substat": True
+    },
+    "FIGHT_PROP_DEFENSE_PERCENT": {
+        "name": "防禦力%",
+        "emoji": "<:DEFENSE_PERCENT:982138218822639626>",
+        "substat": True
+    },
+    "FIGHT_PROP_CRITICAL": {
+        "name": "暴擊率%",
+        "emoji": "<:CRITICAL:982068460731392040>",
+        "substat": True
+    },
+    "FIGHT_PROP_CRITICAL_HURT": {
+        "name": "暴擊傷害%",
+        "emoji": "<:CRITICAL_HURT:982068462081933352>",
+        "substat": True
+    },
+    "FIGHT_PROP_ELEMENT_MASTERY": {
+        "name": "元素精通",
+        "emoji": "<:ELEMENT_MASTERY:982068464938270730>",
+        "substat": True
+    },
+    "FIGHT_PROP_CHARGE_EFFICIENCY": {
+        "name": "元素充能%",
+        "emoji": "<:CHARGE_EFFICIENCY:982068459179503646>",
+        "substat": True
+    },
+    "FIGHT_PROP_BASE_HP": {
+        "name": "基礎生命值",
+        "emoji": "",
+        "substat": False
+    },
+    "FIGHT_PROP_BASE_ATTACK": {
+        "name": "基礎攻擊力",
+        "emoji": "",
+        "substat": False
+    },
+    "FIGHT_PROP_BASE_DEFENSE": {
+        "name": "基礎防禦力",
+        "emoji": "",
+        "substat": False
+    },
+    "FIGHT_PROP_HEAL_ADD": {
+        "name": "治療加成%",
+        "emoji": "<:HEAL_ADD:982138224170401853>",
+        "substat": False
+    },
+    "FIGHT_PROP_PHYSICAL_ADD_HURT": {
+        "name": "物傷加成%",
+        "emoji": "<:PHYSICAL_ADD_HURT:982138230923231242>",
+        "substat": False
+    },
+    "FIGHT_PROP_FIRE_ADD_HURT": {
+        "name": "火傷加成%",
+        "emoji": "<:FIRE_ADD_HURT:982138221569900585>",
+        "substat": False
+    },
+    "FIGHT_PROP_ELEC_ADD_HURT": {
+        "name": "雷傷加成%",
+        "emoji": "<:ELEC_ADD_HURT:982138220248711178>",
+        "substat": False
+    },
+    "FIGHT_PROP_WATER_ADD_HURT": {
+        "name": "水傷加成%",
+        "emoji": "<:WATER_ADD_HURT:982138233813098556>",
+        "substat": False
+    },
+    "FIGHT_PROP_GRASS_ADD_HURT": {
+        "name": "草傷加成%",
+        "emoji": "<:GRASS_ADD_HURT:982138222891110432>",
+        "substat": False
+    },
+    "FIGHT_PROP_WIND_ADD_HURT": {
+        "name": "風傷加成%",
+        "emoji": "<:WIND_ADD_HURT:982138235239137290>",
+        "substat": False
+    },
+    "FIGHT_PROP_ROCK_ADD_HURT": {
+        "name": "岩傷加成%",
+        "emoji": "<:ROCK_ADD_HURT:982138232391237632>",
+        "substat": False
+    },
+    "FIGHT_PROP_ICE_ADD_HURT": {
+        "name": "冰傷加成%",
+        "emoji": "<:ICE_ADD_HURT:982138229140635648>",
+        "substat": False
+    },
+    "FIGHT_PROP_BASE_SPEED": {
+        "name": "基礎移動速度",
+        "emoji": "",
+        "substat": False
+    },
+    "FIGHT_PROP_SPEED_PERCENT": {
+        "name": "移動速度%",
+        "emoji": "",
+        "substat": False
+    }
 }
