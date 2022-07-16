3c1a4635676bfbd188fd07d62993518602072363
seriaati <seria.ati@email.com>
2022-07-16 20:54:22 +0800

    fixed #30 部分摸魚系統的魚所使用的圖片無效

--- data/fish/fish_data.py
+++ data/fish/fish_data.py
@@ -5,7 +5,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://www.ocean-treasure.com/wp-content/uploads/2021/06/Milkfish.jpg'
+        'image_url': 'https://i.imgur.com/L4grW7M.jpg'
     },
     '鮭魚': {
         'flow': 2,
@@ -13,7 +13,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://cdn.discordapp.com/avatars/329643343879471104/43da237aff2393e47d7187a2ba8b6827.png?size=1024'
+        'image_url': 'https://i.imgur.com/r7xp3v0.png'
     },
     '鱈魚': {
         'flow': 2,
@@ -21,7 +21,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://seafoodfriday.hk/wp-content/uploads/2021/08/Cod-Fillet-1.jpg'
+        'image_url': 'https://i.imgur.com/hA2cDUi.jpg'
     },
     '鮪魚': {
         'flow': 2,
@@ -29,7 +29,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://storage.googleapis.com/opinion-cms-cwg-tw/article/201806/article-5b2239595cee6.jpg'
+        'image_url': 'https://i.imgur.com/JhC9N8q.jpg'
     },
     '鰻魚': {
         'flow': 2,
@@ -37,7 +37,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://www.boilingtime.com/img/0630/f.jpg'
+        'image_url': 'https://i.imgur.com/ZIFr64Z.jpg'
     },
     '企鵝': {
         'flow': 3,
@@ -45,7 +45,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://i.pinimg.com/originals/f2/38/ce/f238ce8da599e3beb5e3f85441083ea2.gif'
+        'image_url': 'https://i.imgur.com/36bauQv.gif'
     },
     '兔兔': {
         'flow': 3,
@@ -53,7 +53,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://i.pinimg.com/236x/f6/de/7f/f6de7f2ec162913fa46704ccf9cb0bd6.jpg'
+        'image_url': 'https://i.imgur.com/n3uOc3b.jpg'
     },
     '雞雞': {
         'flow': 3,
@@ -61,7 +61,7 @@ fish_data = {
         'type_0': True,
         'cute': False,
         'verb': None,
-        'image_url': 'https://c.tenor.com/V4-1u-unJYkAAAAC/chicken-sad.gif'
+        'image_url': 'https://i.imgur.com/fTQb7kB.gif'
     },
     '龍蝦': {
         'flow': 5,
@@ -69,7 +69,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '鉗到了',
-        'image_url': 'https://seafoodfriday.hk/wp-content/uploads/2021/08/Red-Lobster-1-1536x1536.jpg'
+        'image_url': 'https://i.imgur.com/YQLW9UM.jpg'
     },
     '螃蟹': {
         'flow': 5,
@@ -77,7 +77,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '鉗到了',
-        'image_url': 'https://www.freshexpressonline.com/media/catalog/product/cache/cce444513434d709cad419cac6756dc1/8/0/804001004.jpg'
+        'image_url': 'https://i.imgur.com/CHKRi6P.jpg'
     },
     '心海': {
         'flow': 7,
@@ -85,7 +85,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '打飛了',
-        'image_url': 'https://assets2.rockpapershotgun.com/genshin-impact-sangonomiya-kokomi.jpg/BROK/thumbnail/1200x1200/quality/100/genshin-impact-sangonomiya-kokomi.jpg'
+        'image_url': 'https://i.imgur.com/MPlHEJX.jpg'
     },
     '大白鯊': {
         'flow': 10,
@@ -93,7 +93,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '咬了幾口',
-        'image_url': 'https://static01.nyt.com/images/2020/08/12/multimedia/00xp-shark/00xp-shark-mediumSquareAt3X.jpg'
+        'image_url': 'https://i.imgur.com/MglF5Sx.jpg'
     },
     '大象': {
         'flow': 15,
@@ -101,7 +101,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '踩了幾腳',
-        'image_url': 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/2-african-elephant-closeup-square-susan-schmitz.jpg'
+        'image_url': 'https://i.imgur.com/OEjQR7O.jpg'
     },
     '達達利鴨': {
         'flow': 20,
@@ -109,7 +109,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '踢了幾腳',
-        'image_url': 'https://c.tenor.com/blHN79J-floAAAAd/ducktaglia-duck.gif'
+        'image_url': 'https://i.imgur.com/gkCHP1Z.gif'
     },
     '抹香鯨': {
         'flow': 20,
@@ -117,7 +117,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '鯨爆了',
-        'image_url': 'https://i.natgeofe.com/n/8084965e-1dfc-47eb-b0c5-e4f86ee65c82/sperm-whale_thumb.jpg'
+        'image_url': 'https://i.imgur.com/WfXpYLw.jpg'
     },
     '蝦蝦': {
         'flow': 25,
@@ -133,7 +133,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '咬到了',
-        'image_url': 'http://pm1.narvii.com/7976/5fa6fb5fd68721c59702d97677b45a78692b0765r1-1050-1050v2_uhq.jpg'
+        'image_url': 'https://i.imgur.com/UoLuOa6.jpg'
     },
     '神子': {
         'flow': 25,
@@ -141,7 +141,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '調教了',
-        'image_url': 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0f0e05ad-e6a0-44a2-907b-bc681efe3066/dex25ww-10b366dd-7bd9-4656-a18f-33c4420b69c1.jpg/v1/fill/w_894,h_894,q_70,strp/chibi_yae_miko_fox_by_yohchii_dex25ww-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTE4MSIsInBhdGgiOiJcL2ZcLzBmMGUwNWFkLWU2YTAtNDRhMi05MDdiLWJjNjgxZWZlMzA2NlwvZGV4MjV3dy0xMGIzNjZkZC03YmQ5LTQ2NTYtYTE4Zi0zM2M0NDIwYjY5YzEuanBnIiwid2lkdGgiOiI8PTExODEifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.n9BtnmHH_U3yJVIBKBHP6gX6kf0C2TazWhV6ZgTxcRQ'
+        'image_url': 'https://i.imgur.com/VkbCdGs.jpg'
     },
     '安妮亞': {
         'flow': 30,
@@ -149,7 +149,7 @@ fish_data = {
         'type_0': False,
         'cute': False,
         'verb': '揍了一拳',
-        'image_url': 'https://img-9gag-fun.9cache.com/photo/aOQRZ66_460s.jpg'
+        'image_url': 'https://i.imgur.com/dKCakCq.jpg'
     },
     '綾華': {
         'flow': 30,
@@ -157,7 +157,7 @@ fish_data = {
         'type_0': False,
         'cute': True,
         'verb': '扇了一巴掌',
-        'image_url': 'https://i.pinimg.com/736x/df/b2/7a/dfb27a74b80ab17e975b8a2997d58faf.jpg'
+        'image_url': 'https://i.imgur.com/ayE8RTR.jpg'
     },
     '02': {
         'flow': 30,
