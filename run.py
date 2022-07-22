45558592b00af36d7d5cf098968e2a1618217f2c
seriaati <seria.ati@email.com>
2022-07-23 06:13:07 +0800

    更改 run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # shenhe-bot by seria
 
-import getpass
 import os
 import sys
 import traceback
 from pathlib import Path
+import getpass
 
 import aiohttp
 import aiosqlite
@@ -15,30 +15,25 @@ from dotenv import load_dotenv
 from enkanetwork import EnkaNetworkAPI
 from pyppeteer import launch
 
-from cogs.flow import FlowCog
 from cogs.gvaway import GiveAwayCog
-from cogs.roles import ReactionRoles
-from cogs.welcome import WelcomeCog
 from debug import DebugView
 from utility.db_utils import DbUtils
 from utility.utils import errEmbed, log
 
 load_dotenv()
-user_name = getpass.getuser()
-if user_name == "alice":
-    token = os.getenv('main')
-    prefix = ['!', '！']
-    guild = 778804551972159489
-    application_id = 956049912699715634
-    debug_toggle = False
-else:
-    token = os.getenv('dev')
-    prefix = ['!']
-    guild = 778804551972159489
-    application_id = 957621570128449626
-    debug_toggle = True
 
-# 前綴, token, intents
+user_name = getpass.getuser()
+
+if user_name == 'seria':
+    token = os.getenv('DEV_TOKEN')
+    debug = True
+    application_id = os.getenv('DEV_APP_ID')
+else:
+    token = os.getenv('MAIN_TOKEN')
+    debug = False
+    application_id = os.getenv('MAIN_APP_ID')
+
+prefix = ['?']
 intents = Intents.default()
 intents.members = True
 intents.reactions = True
@@ -51,31 +46,25 @@ class ShenheBot(commands.Bot):
         super().__init__(
             command_prefix=prefix,
             intents=intents,
-            application_id=application_id,
-            owner_ids=[289597294075183114,
-                       410036441129943050, 831883841417248778]
+            application_id=application_id
         )
 
     async def setup_hook(self) -> None:
+        # bot variables
         self.session = aiohttp.ClientSession()
         self.db = await aiosqlite.connect('main.db')
         self.browser = await launch({'headless': True, 'autoClose': False, "args": ['--proxy-server="direct://"', '--proxy-bypass-list=*', '--no-sandbox', '--start-maximized']})
-        self.debug_toggle = debug_toggle
+        self.debug = debug
         self.enka_client = EnkaNetworkAPI(lang='cht')
+        # load jishaku
         await self.load_extension('jishaku')
+        # load cogs
         for filepath in Path('./cogs').glob('**/*.py'):
             cog_name = Path(filepath).stem
             await self.load_extension(f'cogs.{cog_name}')
+        # load persistent views
         if not self.debug_toggle:
-            self.add_view(FlowCog.AcceptView(self.db, self))
-            self.add_view(FlowCog.ConfirmView(self.db))
             self.add_view(GiveAwayCog.GiveAwayView(self.db, self))
-            self.add_view(ReactionRoles.WorldLevelView())
-            self.add_view(ReactionRoles.RoleView())
-            self.add_view(ReactionRoles.NationalityChooser([1, 2, 3]))
-            self.add_view(WelcomeCog.AcceptRules(self.db))
-            self.add_view(WelcomeCog.StartTutorial(self.db))
-            self.add_view(WelcomeCog.Welcome(None))
 
     async def on_ready(self):
         await self.change_presence(
@@ -96,13 +85,6 @@ class ShenheBot(commands.Bot):
         error = getattr(error, 'original', error)
         if isinstance(error, ignored):
             return
-        if isinstance(error, commands.DisabledCommand):
-            await ctx.send(f'{ctx.command} has been disabled.')
-        elif isinstance(error, commands.NoPrivateMessage):
-            try:
-                await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
-            except HTTPException:
-                pass
         else:
             print('Ignoring exception in command {}:'.format(
                 ctx.command), file=sys.stderr)
@@ -117,22 +99,14 @@ class ShenheBot(commands.Bot):
 
 
 bot = ShenheBot()
+
+
 tree = bot.tree
-
-
 @tree.error
 async def err_handle(i: Interaction, e: app_commands.AppCommandError):
-    if isinstance(e, app_commands.errors.MissingRole):
-        embed = errEmbed(message='你不是小雪團隊的一員').set_author(
-            name='權限不足', icon_url=i.user.avatar)
-        if i.response._responded:
-            await i.edit_original_message(embed=embed)
-        else:
-            await i.response.send_message(embed=embed, ephemeral=True)
-    else:
-        seria = i.client.get_user(410036441129943050)
-        view = DebugView(traceback.format_exc())
-        embed = errEmbed(message=f'```py\n{e}\n```').set_author(
-            name='未知錯誤', icon_url=i.user.avatar)
-        await i.channel.send(content=f'{seria.mention} 系統已將錯誤回報給小雪, 請耐心等待修復', embed=embed, view=view)
+    traceback_message = traceback.format_exc()
+    view = DebugView(traceback_message)
+    embed = errEmbed('發生了未知的錯誤, 請至[申鶴的 issue 頁面](https://github.com/seriaati/shenhe_bot/issues)回報這個錯誤').set_author(
+        name='未知錯誤', icon_url=i.user.avatar)
+    await i.channel.send(embed=embed, view=view)
 bot.run(token)
