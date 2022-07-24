98e39affc0a135baa3d5dee48f2a238846747c3c
seriaati <seria.ati@email.com>
2022-07-24 17:41:24 +0800

    移除一些不必要的檔案

--- data/game/equip_types.py
+++
@@ -1,7 +0,0 @@
-equip_types = {
-    'Flower': '<:Flower_of_Life:982167959717945374>',
-    'Feather': '<:Plume_of_Death:982167959915077643>',
-    'Sands': '<:Sands_of_Eon:982167959881547877>',
-    'Goblet': '<:Goblet_of_Eonothem:982167959835402240>',
-    'Circlet': '<:Circlet_of_Logos:982167959692787802>'
-}
