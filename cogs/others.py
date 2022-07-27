5c70fb527a9108c680e1623ed20fce1b546586ed
seriaati <seria.ati@email.com>
2022-07-27 18:17:33 +0800

    新增 /update 指令

--- cogs/others.py
+++ cogs/others.py
@@ -1,11 +1,12 @@
 from typing import Any
-
+import json
 import aiosqlite
 from debug import DefaultView
 from discord import Interaction, Locale, SelectOption, app_commands
 from discord.ext import commands
 from discord.ui import Select
-from utility.utils import defaultEmbed, TextMap
+from utility.utils import defaultEmbed, TextMap, errEmbed
+from data.textMap.dc_locale_to_enka import DLE
 
 
 class OthersCog(commands.Cog, name='others'):
@@ -110,6 +111,25 @@ class OthersCog(commands.Cog, name='others'):
         )
         embed.set_author(name='更改語言', icon_url=i.user.avatar)
         await i.response.send_message(embed=embed, view=OthersCog.LangView(i.locale, self.bot.db, user_locale), ephemeral=True)
+        
+    @app_commands.command(name='update更新', description='更新原神資料（管理員用指令）')
+    async def update(self, i: Interaction):
+        if i.user.id != 410036441129943050:
+            return await i.response.send_message(embed=errEmbed(message='你不是小雪本人').set_author(name='生物驗證失敗', icon_url=i.user.avatar), ephemeral=True)
+        await i.response.send_message(embed=defaultEmbed().set_author(name='更新資料開始', icon_url=i.user.avatar))
+        things_to_update = ['avatar', 'weapon', 'material']
+        for thing in things_to_update:
+            dict = {}
+            for lang in list(DLE.values()):
+                async with self.bot.session.get(f'https://api.ambr.top/v2/{lang}/{thing}') as r:
+                    data = await r.json()
+                for character_id, character_info in data['data']['items'].items():
+                    if character_id not in dict:
+                        dict[character_id] = {}
+                    dict[character_id][lang] = character_info['name']
+            with open (f'data/textMap/{thing}.json', 'w+') as f:
+                json.dump(dict, f, indent=4)
+        await i.edit_original_message(embed=defaultEmbed().set_author(name='更新資料完畢', icon_url=i.user.avatar))
     
     
 async def setup(bot: commands.Bot) -> None:
