c35e169d82273bfe4f8106832c6db6dae1eda1f0
seria_ati <seria.ati@gmail.com>
2022-08-11 11:08:26 +0800

    新增 /version

---
+++ data/update/change_log.py
@@ -0,0 +1,16 @@
+change_log = {
+    'v4.6.2':
+        "• 修復自動領獎失效的問題\n"
+        "• 修復 `/build` 按按鈕觸發的錯誤 [#92](https://github.com/seriaati/shenhe_bot/issues/92)\n"
+        "• 翻頁系統新增本地化的錯誤訊息 [#100](https://github.com/seriaati/shenhe_bot/pull/100)\n",
+    'v4.6.1':
+        "• 指令本地化 [#86](https://github.com/seriaati/shenhe_bot/pull/86)\n"
+        "• 排行榜 `/leaderboard` 改寫 [#88](https://github.com/seriaati/shenhe_bot/pull/88)\n"
+        "• 修復 `/build` 不能返回元素選擇的問題 [#89](https://github.com/seriaati/shenhe_bot/issues/89)",
+    'v4.6.0':
+        "• 修復 `/wish setkey` 設置失敗時沒有跳出 function 的問題\n"
+        "• 修復 `/profile` 語言部份的問題 [#77](https://github.com/seriaati/shenhe_bot/issues/77)"
+        "• 修復 `/build` 風屬性無法顯示的問題 [#81](https://github.com/seriaati/shenhe_bot/issues/81)\n"
+        "• `/wish` 祈願系列指令改寫+本地化 [#76](https://github.com/seriaati/shenhe_bot/pull/76)\n"
+        "• 改進了 `/events` [#84](https://github.com/seriaati/shenhe_bot/pull/84)"
+}
