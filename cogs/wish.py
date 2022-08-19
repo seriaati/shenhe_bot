f534b1ad2ef11359fca9608b1cecc6ef3ebf0bb7
seria_ati <seria.ati@gmail.com>
2022-08-19 12:54:15 +0800

    update primo and mora emojis

--- cogs/wish.py
+++ cogs/wish.py
@@ -197,14 +197,14 @@ class WishCog(commands.GroupCog, name='wish'):
             total += banner['total']
         embed = default_embed(
             message=f"{text_map.get(375, i.locale, user_locale)} {total} {text_map.get(376,i.locale, user_locale)}\n"
-            f"{text_map.get(396, i.locale, user_locale)} {160*total} <:primo:958555698596290570> {text_map.get(397, i.locale, user_locale)}")
+            f"{text_map.get(396, i.locale, user_locale)} {160*total} <:PRIMO:1010048703312171099> {text_map.get(397, i.locale, user_locale)}")
 
         for index, banner in enumerate(overview.values()):
             average = (banner['total'] // banner['five_star']
                        ) if banner['five_star'] != 0 else 0
             embed.add_field(
                 name=f'{banner_names[index]}',
-                value=f'• {text_map.get(375, i.locale, user_locale)} **{banner["total"]}** {text_map.get(376, i.locale, user_locale)}\n({banner["total"]*160} <:primo:958555698596290570> {text_map.get(397, i.locale, user_locale)})\n'
+                value=f'• {text_map.get(375, i.locale, user_locale)} **{banner["total"]}** {text_map.get(376, i.locale, user_locale)}\n({banner["total"]*160} <:PRIMO:1010048703312171099> {text_map.get(397, i.locale, user_locale)})\n'
                 f'• 5 <:white_star:982456919224615002>: **{banner["five_star"]}**\n'
                 f'• 4 <:white_star:982456919224615002>: **{banner["four_star"]}**\n'
                 f'• {text_map.get(401, i.locale, user_locale)} **{average}** {text_map.get(402, i.locale, user_locale)}\n'
