6e3034086ec5ccaa6a802405877c7de5c2d6e628
seria_ati <seria.ati@gmail.com>
2022-08-21 06:28:44 +0800

    Revert "move ambr client to run"
    This reverts commit 003dfa6a2ad0218551b849579489e5e675500f48.

--- run.py
+++ run.py
@@ -16,7 +16,6 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from enkanetwork import EnkaNetworkAPI
 from pyppeteer import launch
-from ambr.client import AmbrTopAPI
 
 from apps.text_map.text_map_app import text_map
 from apps.text_map.utils import get_user_locale
@@ -72,7 +71,6 @@ class ShenheBot(commands.Bot):
         self.browser = await launch({'headless': True, 'autoClose': False, "args": ['--proxy-server="direct://"', '--proxy-bypass-list=*', '--no-sandbox', '--start-maximized']})
         self.debug = debug
         self.enka_client = EnkaNetworkAPI()
-        self.ambr_client = AmbrTopAPI(self.session)
         
         # load jishaku
         await self.load_extension('jishaku')
