84fa12d5e7d5f39ab5606cc240baa6c5539a4125
seria_ati <seria.ati@gmail.com>
2022-09-01 19:10:46 +0800

    add sentry capturing for edit view httpexception

--- debug.py
+++ debug.py
@@ -35,6 +35,7 @@ class DefaultView(discord.ui.View):
         except AttributeError:
             log.warning(f"[Attribute Error][Edit View]: [children]{self.children}")
         except discord.HTTPException as e:
+            sentry_sdk.capture_event(e)
             log.warning(f"[HTTPException][Edit View]: [children]{self.children} [code]{e.code} [message]{e.text}")
         except Exception as e:
             sentry_sdk.capture_event(e)
