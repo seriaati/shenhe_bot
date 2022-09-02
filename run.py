9d47263862ede87c18ec166e27a7f7381ff6cbb9
seria_ati <seria.ati@gmail.com>
2022-09-03 07:25:22 +0800

    chrome path

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ class ShenheBot(commands.Bot):
                     "--no-sandbox",
                     "--start-maximized",
                 ],
-                "executablePath": "chrome-linux"
+                "executablePath": "../shenhe_bot/chrome-linux"
             }
         )
         self.debug = debug
