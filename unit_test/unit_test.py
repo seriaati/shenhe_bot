a5aa89f32324fba41a05cbda8ed98b1a41804b66
seriaati <seria.ati@gmail.com>
2022-09-08 15:53:04 +0800

    Create unit_test.py

---
+++ unit_test/unit_test.py
@@ -0,0 +1,35 @@
+commands = ["abyss",
+            "activity",
+            "area",
+            "build",
+            "calc character",
+            "characters",
+            "check",
+            "claim",
+            "devmsg",
+            "diary",
+            "events",
+            "farm",
+            "help",
+            "instantclaim",
+            "lang",
+            "leaderboard",
+            "profile",
+            "search",
+            "stats",
+            "todo",
+            "version",
+            "waifu sfw",
+            "waifu nsfw",
+            "waifu waifu",
+            "wish history",
+            "wish luck",
+            "wish overview"]
+
+for command in commands:
+    type("/" + command)
+    wait(1)
+    type(Key.ENTER)
+    type(Key.ENTER)
+    type(Key.ENTER)
+    wait(1)
