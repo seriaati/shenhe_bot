b7a2b15808fffcfa8e8a7aed75c2d493a9fc3ace
seriaati <seria.ati@gmail.com>
2022-09-29 22:28:01 +0800

    small optimizations

--- UI_elements/genshin/EventTypeChooser.py
+++ UI_elements/genshin/EventTypeChooser.py
@@ -2,7 +2,7 @@ from discord.ui import Select
 from discord import Interaction, Embed, SelectOption, Locale
 from typing import Any, Dict, List
 from apps.text_map.text_map_app import text_map
-        
+    
 class Select(Select):
     def __init__(self, options: List[SelectOption], embeds: Dict[int, List[Embed]], locale: Locale, user_locale: str | None) -> None:
         super().__init__(options=options, placeholder=text_map.get(409, locale, user_locale))
@@ -10,5 +10,5 @@ class Select(Select):
         
     async def callback(self, i: Interaction) -> Any:
         self.view.current_page = 0
-        self.view.embeds = self.embeds[int(self.values[0])]
-        await i.response.edit_message(embed=self.embeds[int(self.values[0])][0], view=self.view)
+        self.view.embeds = self.embeds[self.values[0]]
+        await i.response.edit_message(embed=self.view.embeds[0], view=self.view)
