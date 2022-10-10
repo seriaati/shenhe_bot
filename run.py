df59841e15f7aa59ff2df9c00cdd342d67ffc66b
seriaati <seria.ati@gmail.com>
2022-10-10 12:35:06 +0800

    optimize translator

--- run.py
+++ run.py
@@ -49,6 +49,8 @@ class Translator(app_commands.Translator):
     ) -> Optional[str]:
         try:
             text = text_map.get(string.extras["hash"], locale)
+            if text == '':
+                return None
             if len(text.split(' ')) == 1: # is a word
                 return text.lower()
             else: # is a setence
