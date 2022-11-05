4e0cf36be6069c928b07448027dda887ce1eb57d
seriaati <seria.ati@gmail.com>
2022-11-05 22:56:34 +0800

    add asset file

--- asset.py
+++ asset.py
@@ -1,2 +1,8 @@
 loader = "https://i.imgur.com/V76M9Wa.gif"
 info_emoji = "<:info_icon:1037635585562660895>"
+link_emoji = "<:song_link:1021667672225763419>"
+import_emoji = "<:import2:1037927911690338324>"
+export_emoji = "<:export2:1037927909932929144>"
+back_emoji = "<:left:982588994778972171>"
+genshin_emoji = "<:genshin_icon:1025630733068423169>"
+shenhe_emoji = "<:shenhe:1037959470271188992>"
