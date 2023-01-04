eb56a0c7d51c6a9db2321da2e8e5ebf12324f90b
seriaati <seria.ati@gmail.com>
2023-01-04 08:14:47 +0800

    update get_dt_now()

--- utility/utils.py
+++ utility/utils.py
@@ -92,7 +92,7 @@ async def get_user_appearance_mode(user_id: int, db: aiosqlite.Connection) -> bo
 
 def get_dt_now() -> datetime:
     """Get current datetime in UTC+8 timezone."""
-    return datetime.now().astimezone(tz=timezone(timedelta(hours=8)))
+    return datetime.now() + timedelta(hours=8)
 
 def add_bullet_points(texts: List[str]) -> str:
     """Add bullet points to a list of texts."""
