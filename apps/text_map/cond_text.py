2c0dd31dfbc682d3d49af1dcf852a4d34dcede2b
seriaati <seria.ati@gmail.com>
2023-01-09 18:58:37 +0800

    fixes

--- apps/text_map/cond_text.py
+++ apps/text_map/cond_text.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from discord import Locale
 import yaml
 
 from apps.text_map.convert_locale import to_paths
@@ -15,7 +15,7 @@ class CondText:
                         self.data[lang] = {}
                     self.data[lang][file] = yaml.safe_load(f)
     
-    def get_text(self, lang: str, file: Literal['artifact', 'build', 'character',' weapon'], key: str) -> str:
+    def get_text(self, lang: str | Locale, file: str, key: str) -> str:
         lang = to_paths(lang)
         if lang not in self.data:
             if lang == 'zh-CN':
