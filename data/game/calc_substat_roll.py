a87b12f8183f96b61a49180e00173370503cb921
seriaati <seria.ati@gmail.com>
2023-01-28 12:42:50 +0800

    fix: sub stat roll calc

--- data/game/calc_substat_roll.py
+++ data/game/calc_substat_roll.py
@@ -12,7 +12,7 @@ def calculate_substat_roll(prop_id: str, value: float) -> int:
         for p_id, table in sub_stats.items():
             if p_id == prop_id:
                 for sub_stat_value, possible_rolls in table.items():
-                    if float(sub_stat_value.replace(",", "")) == value:
+                    if float(sub_stat_value.replace(", ", "")) == value:
                         for possible_roll in possible_rolls:
                             return len(possible_roll)
 
