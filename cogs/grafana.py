fa5ae3114eb5d719d80d4c8cd08d813c6ddbef79
seriaati <seria.ati@gmail.com>
2023-02-03 19:38:04 +0800

    feat: add grafana cog

---
+++ cogs/grafana.py
@@ -0,0 +1,9 @@
+from discord.ext.prometheus import PrometheusCog
+from discord.ext import commands
+
+class GrafanaCog(commands.Cog):
+    def __init__(self, bot: commands.AutoShardedBot):
+        self.bot = bot
+
+async def setup(bot: commands.AutoShardedBot):
+    await bot.add_cog(PrometheusCog(bot))
