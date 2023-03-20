bf7ce546d14ab565ae91526084fce01ea2ffbb63
seriaati <seria.ati@gmail.com>
2023-03-20 18:58:05 +0800

    fix: multiple imports of same name

--- ui/genshin/UIDCommand.py
+++ ui/genshin/UIDCommand.py
@@ -1,8 +1,9 @@
-from base_ui import BaseView
-from discord import Locale, Interaction, ButtonStyle
+from discord import ButtonStyle, Interaction, Locale
+from discord.ui import Button
+
 import config
-from discord.ui import Button, Button
 from apps.text_map.text_map_app import text_map
+from base_ui import BaseView
 
 
 class View(BaseView):
