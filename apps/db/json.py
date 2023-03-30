5cafbc719443f2d8f9f4ac820e70775b8c25a626
seriaati <seria.ati@gmail.com>
2023-03-30 10:09:06 +0800

    fix: empty dict not returning none

--- apps/db/json.py
+++ apps/db/json.py
@@ -1,16 +1,18 @@
 import json
-from typing import Dict
+from typing import Any, Dict, Optional
 
 import asyncpg
 
 
-async def read_json(pool: asyncpg.Pool, file_name: str) -> Dict:
+async def read_json(pool: asyncpg.Pool, file_name: str) -> Optional[Dict[str, Any]]:
     """Reads a json file from the database and returns it as a dict"""
     val = await pool.fetchval("SELECT file FROM json WHERE file_name = $1", file_name)
+    if not val or val == "{}":
+        return None
     return json.loads(val)
 
 
-async def write_json(pool: asyncpg.Pool, file_name: str, data: Dict):
+async def write_json(pool: asyncpg.Pool, file_name: str, data: Dict) -> None:
     """Writes a json file to the database"""
     await pool.execute(
         "INSERT INTO json (file_name, file) VALUES ($1, $2) ON CONFLICT (file_name) DO UPDATE SET file = $2",
@@ -19,6 +21,6 @@ async def write_json(pool: asyncpg.Pool, file_name: str, data: Dict):
     )
 
 
-async def delete_json(pool: asyncpg.Pool, file_name: str):
+async def delete_json(pool: asyncpg.Pool, file_name: str) -> None:
     """Deletes a json file from the database"""
     await pool.execute("DELETE FROM json WHERE file_name = $1", file_name)
