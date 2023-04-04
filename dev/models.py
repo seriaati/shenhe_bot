83c5caa39f8a4b7d7197f1a8355ba3782a5b3c94
seriaati <seria.ati@gmail.com>
2023-04-04 20:40:33 +0800

    modify: move enka models

--- dev/models.py
+++ dev/models.py
@@ -356,28 +356,3 @@ class Inter(discord.Interaction):
 class TalentBoost(Enum):
     BOOST_E = "boost_e"
     BOOST_Q = "boost_q"
-
-
-class EnkaPlayerInfo(BaseModel):
-    nickname: str
-    level: int
-    signature: str
-    world_level: int = Field(alias="worldLevel")
-    name_card_id: int = Field(alias="nameCardId")
-    achievement_num: int = Field(alias="finishAchievementNum")
-    abyss_floor: int = Field(alias="towerFloorIndex")
-    abyss_chamber: int = Field(alias="towerLevelIndex")
-
-
-class EnkaInfoResponse(BaseModel):
-    player_info: EnkaPlayerInfo = Field(alias="playerInfo")
-    uid: int
-    ttl: int
-
-    @validator("player_info", pre=True, always=True, allow_reuse=True)
-    def parse_player_info(cls, v):
-        return EnkaPlayerInfo(**v)
-
-    @validator("uid", pre=True, always=True, allow_reuse=True)
-    def parse_uid(cls, v):
-        return int(v)
