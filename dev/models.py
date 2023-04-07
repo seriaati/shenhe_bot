6d7937d2edb08662172631580f77e9cc3a95c9ee
seriaati <seria.ati@gmail.com>
2023-04-07 21:04:05 +0800

    fix: remove model validation

--- dev/models.py
+++ dev/models.py
@@ -299,7 +299,7 @@ class OriginalInfo(BaseModel):
         arbitrary_types_allowed = True
 
 
-class FarmData(BaseModel):
+class FarmData:
     domain: ambr.Domain
     characters: typing.List[ambr.Character] = []
     weapons: typing.List[ambr.Weapon] = []
