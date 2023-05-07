a7ea7af1855d4192e8747f700d631c61343c0132
seriaati <seria.ati@gmail.com>
2023-05-07 23:25:31 +0800

    fix(pc_icon): self-fix `pc_icon` url when it is an empty string

--- apps/draw/main_funcs.py
+++ apps/draw/main_funcs.py
@@ -14,7 +14,8 @@ import dev.models as models
 from ambr import Material
 from apps.db.json import read_json, write_json
 from apps.wish.models import RecentWish, WishData
-from utils import calculate_time, compress_image_util, download_images, extract_urls
+from utils import (calculate_time, compress_image_util, download_images,
+                   extract_urls)
 
 
 @calculate_time
@@ -360,7 +361,11 @@ async def character_summary_card(
     urls = [c.weapon.icon for c in character_list]
     pc_icons = await read_json(pool, "genshin/pc_icons.json")
 
-    if not pc_icons or any(str(c.id) not in pc_icons for c in character_list):
+    if (
+        not pc_icons
+        or any(str(c.id) not in pc_icons for c in character_list)
+        or any(not pc_icons[str(c.id)] for c in character_list)
+    ):
         client = genshin.Client()
         fields = await client.get_lineup_fields()
         pc_icons = {
