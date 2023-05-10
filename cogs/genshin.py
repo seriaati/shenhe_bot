e1f145f96833462c071fcaeefccb1af93089c675
seriaati <seria.ati@gmail.com>
2023-05-10 18:36:57 +0800

    refactor(claim): rewrite a little bit and reduce uncessary queries

--- cogs/genshin.py
+++ cogs/genshin.py
@@ -26,15 +26,9 @@ from apps.genshin import enka, hoyolab, leaderboard
 from apps.genshin_data import abyss
 from apps.text_map import convert_locale, text_map
 from data.cards.dice_element import get_dice_emoji
-from utils import (
-    disable_view_items,
-    get_character_emoji,
-    get_uid,
-    get_uid_region_hash,
-    get_user_lang,
-    get_user_theme,
-    log,
-)
+from utils import (disable_view_items, get_character_emoji, get_uid,
+                   get_uid_region_hash, get_user_lang, get_user_theme, log)
+from utils.db import get_user_daily_checkin
 from utils.paginators import GeneralPaginator
 
 load_dotenv()
@@ -319,7 +313,14 @@ class GenshinCog(commands.Cog, name="genshin"):
     )
     async def claim(self, inter: discord.Interaction):
         i: models.Inter = inter  # type: ignore
-        await ui.daily_reward.return_claim_reward(i, self.genshin_app)
+        lang = await get_user_lang(i.user.id, self.bot.pool) or i.locale
+        uid = await get_uid(i.user.id, self.bot.pool)
+        daily_checkin = await get_user_daily_checkin(i.user.id, self.bot.pool)
+        if uid is None:
+            raise exceptions.UIDNotFound
+        
+        view = ui.daily_reward.View(lang, self.genshin_app, uid, daily_checkin)
+        await view.start(i)
 
     @checks.check_cookie()
     @app_commands.command(
--- ui/genshin/daily_reward.py
+++ ui/genshin/daily_reward.py
@@ -12,7 +12,7 @@ from apps.genshin import GenshinApp
 from apps.text_map import text_map
 from dev.base_ui import BaseView
 from dev.models import DefaultEmbed, ErrorEmbed, Inter
-from utils import divide_chunks, get_dt_now, get_user_lang
+from utils import divide_chunks, get_dt_now
 
 
 class View(BaseView):
@@ -31,6 +31,49 @@ class View(BaseView):
         self.add_item(ClaimRewardOff(self.locale, not daily_checkin))
         self.add_item(ClaimReward(self.locale))
 
+    async def start(self, i: Inter):
+        try:
+            await i.response.defer()
+        except InteractionResponded:
+            pass
+
+        now = get_dt_now()
+        day_in_month = calendar.monthrange(now.year, now.month)[1]
+        shenhe_user = await self.genshin_app.get_user_cookie(i.user.id, i.user.id, i.locale)
+
+        try:
+            _, claimed_rewards = await shenhe_user.client.get_reward_info()
+        except genshin.errors.InvalidCookies:
+            embed = ErrorEmbed(description=text_map.get(35, self.locale))
+            embed.set_author(
+                name=text_map.get(36, self.locale),
+                icon_url=i.user.display_avatar.url,
+            )
+            return await i.followup.send(embed=embed)
+
+        embed = DefaultEmbed(
+            description=f"{text_map.get(606, self.locale)}: {claimed_rewards}/{day_in_month}\n"
+        )
+        embed.set_author(
+            name=text_map.get(604, self.locale),
+            icon_url=i.user.display_avatar.url,
+        )
+        embed.set_footer(text=text_map.get(769, self.locale))
+
+        values = []
+        async for reward in shenhe_user.client.claimed_rewards(limit=claimed_rewards):
+            values.append(
+                f"{reward.time.month}/{reward.time.day} - {reward.name} x{reward.amount}\n"
+            )
+        divided_value = list(divide_chunks(values, 10))
+        for index, val in enumerate(divided_value):
+            r = "".join(val)
+            embed.add_field(name=f"{text_map.get(605, self.locale)} ({index+1})", value=r)
+
+        view = View(self.locale, self.genshin_app, shenhe_user.uid, shenhe_user.daily_checkin)
+        view.message = await i.edit_original_response(embed=embed, view=view)
+        view.author = i.user
+
 
 class ClaimReward(Button):
     def __init__(self, locale: Locale | str):
@@ -42,11 +85,10 @@ class ClaimReward(Button):
         r = await self.view.genshin_app.claim_daily_reward(
             i.user.id, i.user.id, i.locale
         )
-        for item in self.view.children:
-            item.disabled = True  # type: ignore
+        self.view.disable_items()
         await i.edit_original_response(embed=r.result, view=self.view)
         await asyncio.sleep(2)
-        await return_claim_reward(i, self.view.genshin_app)
+        await self.view.start(i)
 
 
 class ClaimRewardOn(Button):
@@ -64,11 +106,7 @@ class ClaimRewardOn(Button):
             i.user.id,
             self.view.uid,
         )
-
-        for item in self.view.children:
-            item.disabled = True  # type: ignore
-
-        await return_claim_reward(i, self.view.genshin_app)
+        await self.view.start(i)
 
 
 class ClaimRewardOff(Button):
@@ -86,57 +124,4 @@ class ClaimRewardOff(Button):
             i.user.id,
             self.view.uid,
         )
-
-        for item in self.view.children:
-            item.disabled = True  # type: ignore
-
-        await return_claim_reward(i, self.view.genshin_app)
-
-
-async def return_claim_reward(i: Inter, genshin_app: GenshinApp):
-    try:
-        await i.response.defer()
-    except InteractionResponded:
-        pass
-
-    locale = await get_user_lang(i.user.id, i.client.pool) or i.locale  # type: ignore
-    now = get_dt_now()
-    day_in_month = calendar.monthrange(now.year, now.month)[1]
-    shenhe_user = await genshin_app.get_user_cookie(i.user.id, i.user.id, i.locale)
-
-    try:
-        _, claimed_rewards = await shenhe_user.client.get_reward_info()
-    except genshin.errors.InvalidCookies:
-        embed = ErrorEmbed(description=text_map.get(35, locale))
-        embed.set_author(
-            name=text_map.get(36, locale),
-            icon_url=i.user.display_avatar.url,
-        )
-        return await i.followup.send(embed=embed)
-
-    embed = DefaultEmbed(
-        description=f"{text_map.get(606, locale)}: {claimed_rewards}/{day_in_month}\n"
-    )
-    embed.set_author(
-        name=text_map.get(604, locale),
-        icon_url=i.user.display_avatar.url,
-    )
-    embed.set_footer(text=text_map.get(769, locale))
-
-    values = []
-    async for reward in shenhe_user.client.claimed_rewards(limit=claimed_rewards):
-        values.append(
-            f"{reward.time.month}/{reward.time.day} - {reward.name} x{reward.amount}\n"
-        )
-    divided_value = list(divide_chunks(values, 10))
-    for index, val in enumerate(divided_value):
-        r = "".join(val)
-        embed.add_field(name=f"{text_map.get(605, locale)} ({index+1})", value=r)
-
-    view = View(locale, genshin_app, shenhe_user.uid, shenhe_user.daily_checkin)
-    try:
-        await i.response.send_message(embed=embed, view=view)
-    except InteractionResponded:
-        await i.edit_original_response(embed=embed, view=view)
-    view.author = i.user
-    view.message = await i.original_response()
+        await self.view.start(i)
