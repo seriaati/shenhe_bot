188e0279e95351153e89ca75de71f3a575909d9d
seriaati <seria.ati@gmail.com>
2023-05-16 08:41:45 +0800

    remove insert functions

--- apps/db/tables/abyss_board.py
+++ apps/db/tables/abyss_board.py
@@ -61,42 +61,6 @@ class AbyssLeaderboard:
     def __init__(self, pool: asyncpg.Pool):
         self.pool = pool
 
-    async def insert(
-        self,
-        *,
-        uid: int,
-        user_id: int,
-        season: int,
-        name: str,
-        ar: int,
-        icon: str,
-        stars: int,
-        wins: int,
-        runs: int,
-        single_strike: int,
-        floor: str,
-        character: SingleStrikeCharacter,
-    ) -> None:
-        """Insert abyss leaderboard entry"""
-        await self.pool.execute(
-            "INSERT INTO abyss_leaderboard (uid, user_id, season, user_name, level, icon_url, stars_collected, wins, runs, single_strike, floor, const, refine, c_level, c_icon) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12::int, $13::int, $14::int, $15) ON CONFLICT DO NOTHING",
-            uid,
-            user_id,
-            season,
-            name,
-            ar,
-            icon,
-            stars,
-            wins,
-            runs,
-            single_strike,
-            floor,
-            character.constellation,
-            character.refinement,
-            character.level,
-            character.icon,
-        )
-
     async def get_all(
         self, category: Category, season: Optional[int] = None
     ) -> List[AbyssBoardEntry]:
--- apps/db/tables/abyss_chara_board.py
+++ apps/db/tables/abyss_chara_board.py
@@ -24,18 +24,6 @@ class AbyssCharacterLeaderboard:
     def __init__(self, pool: asyncpg.Pool):
         self.pool = pool
 
-    async def insert(
-        self, *, uid: int, season: int, character_ids: typing.List[int], user_id: int
-    ) -> None:
-        """Insert abyss character usage leaderboard entry"""
-        await self.pool.execute(
-            "INSERT INTO abyss_character_leaderboard (uid, season, characters, user_id) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING",
-            uid,
-            season,
-            character_ids,
-            user_id,
-        )
-
     async def get_all(
         self, season: typing.Optional[int] = None
     ) -> typing.List[AbyssCharaBoardEntry]:
