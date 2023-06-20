6f4d5d966054265b89611a5658b6940db15c06fe
seria_ati <seria.ati@gmail.com>
2023-06-20 14:58:06 +0800

    modify: clean up data files

--- data/game/artifact_map.py
+++
@@ -1,7 +0,0 @@
-import json
-
-try:
-    with open("data/game/artifact_map.json", encoding="utf-8", mode="r") as f:
-        artifact_map = json.load(f)
-except FileNotFoundError:
-    artifact_map = {}
