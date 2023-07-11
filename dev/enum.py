1d3e196c3de5ed9ac3252ad669cba358bda5a64a
seria_ati <seria.ati@gmail.com>
2023-07-11 08:42:33 +0800

    feat: add wiki category enums

--- dev/enum.py
+++ dev/enum.py
@@ -41,3 +41,44 @@ class Category(Enum):
     SINGLE_STRIKE = "single_strike_damage"
     CHARACTER_USAGE_RATE = "character_usage_rate"
     FULL_CLEAR = "full_clear"
+
+
+class GenshinWikiCategory(Enum):
+    CHARACTER = "avatar"
+    WEAPON = "weapon"
+    MATERIAL = "material"
+    ARTIFACT = "reliquary"
+    MONSTER = "monster"
+    FOOD = "food"
+    FURNITURE = "furniture"
+    NAME_CARD = "namecard"
+    BOOK = "book"
+    TCG = "tcg"
+
+
+class HSRWikiCategory(Enum):
+    CHARACTER = "avatar"
+    LIGHT_CONE = "equipment"
+    ITEM = "item"
+    RELIC = "relic"
+    BOOK = "book"
+    MESSAGE = "message"
+
+
+CATEGORY_HASHES = {
+    GenshinWikiCategory.CHARACTER: 815,
+    GenshinWikiCategory.WEAPON: 816,
+    GenshinWikiCategory.MATERIAL: 827,
+    GenshinWikiCategory.ARTIFACT: 818,
+    GenshinWikiCategory.MONSTER: 819,
+    GenshinWikiCategory.FOOD: 820,
+    GenshinWikiCategory.FURNITURE: 821,
+    GenshinWikiCategory.NAME_CARD: 822,
+    GenshinWikiCategory.BOOK: 823,
+    GenshinWikiCategory.TCG: 831,
+    HSRWikiCategory.CHARACTER: 815,
+    HSRWikiCategory.LIGHT_CONE: 824,
+    HSRWikiCategory.ITEM: 817,
+    HSRWikiCategory.RELIC: 825,
+    HSRWikiCategory.BOOK: 823,
+    HSRWikiCategory.MESSAGE: 826,
